c06eab85ce5b5540fbb90a56628db93840236af6
Frederik Proske <fproske@gams.com>
2018-09-11 10:31:37 +0200

    interrupt is now handled via processx package again wth kill_tree() method that is supported in the latest version of processx

--- modules/gams_interrupt.R
+++ modules/gams_interrupt.R
@@ -1,13 +1,7 @@
 observeEvent(input$btInterrupt,{
   errMsg <- NULL
   tryCatch({
-    if(serverOS == 'windows'){
-      processx::run(command = 'taskkill', args = c("/F", "/PID", gams$get_pid(), "/T"), windows_hide_window = TRUE)
-    }else if (serverOS %in% c('linux', 'osx')){
-      processx::run(command = 'kill', args = c("-SIGKILL", -gams$get_pid()))
-    }else{
-      stop(sprintf("Operating system: '%s' not supported.", serverOS), call. = FALSE)
-    }
+    gams$kill_tree()
   }, error= function(e){
     flog.error("Problems interrupting the gams call. Error message: %s.", e)
     errMsg <<- lang$errMsg$gamsTerm$desc
