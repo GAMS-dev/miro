e0f2beeae2b27d2da3ff0bade63fa6088c2bd5cb
Frederik Proske <fproske@gams.com>
2018-09-12 15:31:54 +0200

    - added scen_table_view.R (forgot to stage in last commit)

---
+++ modules/scen_table_view.R
@@ -0,0 +1,21 @@
+observeEvent(input[["table_" %+% i]], {
+  # get sheet ID for current scenario
+  j <- as.integer(strsplit(isolate(input[["content.scen_" %+% i]]), "_")[[1]][3])
+  flog.debug("Table view in scenario with id: %d for sheet: %d activated.", i, j)
+  if(isInCompareMode){
+    if(isInSplitView){
+      lapply(2:3, function(k){
+        shinyjs::toggle(paste0("scenTable_", k, "_", j))
+        shinyjs::toggle(paste0("scenGraph_", k, "_", j))
+      })
+    }else{
+      lapply(4:(maxNumberScenarios + 3), function(k){
+        shinyjs::toggle(paste0("scenTable_", k, "_", j))
+        shinyjs::toggle(paste0("scenGraph_", k, "_", j))
+      })
+    }
+  }else{
+    shinyjs::toggle(paste0("scenTable_", i, "_", j))
+    shinyjs::toggle(paste0("scenGraph_", i, "_", j))
+  }
+})
