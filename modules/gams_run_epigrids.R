2f596fd92fa56081553a6dcccd520cc9883f15d0
Frederik Proske <fproske@gams.com>
2018-12-17 14:09:25 +0100

    - paver now works under linux and macos as well - fixed bug where _ in dropdown menus was not ignored in batch mode - all files in model folder are downloaded to job submission script

--- modules/gams_run_epigrids.R
+++ modules/gams_run_epigrids.R
@@ -1,15 +1,3 @@
-filesToDownloadForBatchRun <- c("cases", "calc_PowerFlow.gms", "calc_Ybus.gms", "cost_objective.gms", 
-                                "cost_objective_uc.gms", "extract_data.gms", "extract_data_uc.gms", 
-                                "reactive_limits.gms", "save_domain_info.gms", "save_solution.gms", 
-                                "save_solution_uc.gms", "webui_in.gms", "webui_out.gms", "batch_submission.gms", 
-                                "webui.gms", "loadCSV.gms","epigrids", "epigrids_uc", "epigrids_uc_dc", "dcopf", 
-                                "dcopf_shift", "ic_iv", "ic_polar", "ic_rect",
-                                "iv_acopf", "polar_acopf", "polar_decoupled", "rect_acopf", "uc_dc", 
-                                "uc_iv", "uc_polar", "uc_rect", "ybus_iv_acopf", "ybus_polar_acopf", 
-                                "ybus_polar_decoupled", "ybus_rect_acopf", "DataUtilities")
-
-
-
 output$btBatchAll <- downloadHandler(
   filename = function() {
     tolower(modelName) %+% ".zip"
@@ -35,7 +23,7 @@ output$btBatchAll <- downloadHandler(
     writeLines(scenGmsPar, workDirBatch %+% .Platform$file.sep %+% tolower(modelName) %+% ".gmsb")
 
     # Copy files that are needed to solve model
-    file.copy(paste0(homeDir, .Platform$file.sep, modelDir) %+% filesToDownloadForBatchRun, workDirBatch, recursive = TRUE)
+    file.copy(paste0(homeDir, .Platform$file.sep, modelDir), workDirBatch, recursive = TRUE)
     
     updateProgress(incAmount = 1, detail = lang$nav$dialogBatch$waitDialog$desc)
     zip(file, list.files(recursive = TRUE), compression_level = 6)
