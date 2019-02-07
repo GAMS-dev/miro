5879989986bc12b627b44b1cf94ce5940aa742ba
Frederik Proske <fproske@gams.com>
2019-02-07 11:43:13 +0100

    updated shell script to update configuration files

--- updatemiro.sh
+++ updatemiro.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 cd $HOME/miro
 
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
-if [ "$BASE" != "$LOCAL" ]; then
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date: $BASE == $LOCAL"
+elif [ $LOCAL = $BASE ]; then
     git pull
     grep -m 1 -e "^MIROVersion" app.R|cut -f3 -d" "|xargs|sed 's/\./,/g' > ./doc/latest.ver
     sed -e '/<code class="language-json">/r./conf/config_schema.json' ./doc/schema_template.html >./doc/schema.html
-else
-    echo Up to date: $BASE == $LOCAL
 fi
