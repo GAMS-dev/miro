9661648108eabb0839bbfc41dc1e49881c1c9114
Frederik Proske <fproske@gams.com>
2019-02-17 14:08:48 +0100

    - removed dummy custom renderer - added demo applications zip file as download link

--- updatemiro.sh
+++ updatemiro.sh
@@ -14,6 +14,10 @@ elif [ $LOCAL = $BASE ]; then
     git fetch origin develop
     git reset --hard FETCH_HEAD
     git clean -df
+    cd model
+    zip miroDemoApps.zip pickstock/pickstock.gms pickstock/dowjones2016.csv pickstock/conf/pickstock.json pickstock/static/pickstock.png kport/kport.gms kport/conf/kport.json kport/static/kport.jpg pickstock_live/pickstock_live.gms pickstock_live/conf/pickstock_live.json pickstock_live/static/pickstock-live.png transport/transport.gms transport/conf/transport.json transport/customRenderer/trnsport_custom.R transport/customRenderer/us-states.geojson transport_live/transport_live.gms transport_live/conf/transport_live.json transport_live/customRenderer/trnsport_custom.R transport_live/customRenderer/us-states.geojson
+    mv miroDemoApps.zip ../doc
+    cd ..
     MIRO_VERSION=$(grep -m 1 -e "^MIROVersion" app.R|cut -f3 -d" "|xargs) 
     echo $MIRO_VERSION | sed 's/\./,/g'> ./doc/latest.ver
     sed -e '/<code class="language-json">/r./conf/config_schema.json' ./doc/schema_template.html >./doc/schema.html
