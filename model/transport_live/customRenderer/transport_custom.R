a5cccca449792f8cd6773350b5c30845ab0d1701
Frederik Proske <fproske@gams.com>
2019-06-03 13:46:11 +0200

    - removed sf dependency for transport_live - adjusted dow vs index chart in model pickstock_live

--- model/transport_live/customRenderer/transport_custom.R
+++ model/transport_live/customRenderer/transport_custom.R
@@ -10,35 +10,21 @@ trnsport1Output <- function(id, height = NULL, options = NULL, path = NULL){
 }
 
 renderTrnsport1 <- function(input, output, session, data, options = NULL, path = NULL){
-  
-  tryCatch({
-    mapData <- read_sf(file.path(path, options$geojsonloc))
-  }, error = function(e){
-    stop(paste0("Problems reading GEOJSON file. Error message: ", e), call. = FALSE)
-  })
-  
-  labels <- mapData$name
-  
-  # generate a color palette
-  getPalette <- colorRampPalette(brewer.pal(12, "Set3"))
-  pal        <- colorFactor(palette = getPalette(length(unique(labels))), mapData$id)
-  
   #generate map
-  map <- leaflet(mapData) %>%
-    addTiles() %>%
-    setView(lng = data$lngp[[1]], lat = data$latp[[1]], zoom = 5) %>%
-    addMarkers(
-      lng = data$lngp, lat = data$latp,
-      label = paste0(data$i, " (capacity: ", data$cap, ")"),
-      labelOptions = labelOptions(noHide = T, textsize = "22px", style = list("background-color" = "rgb(243, 150, 25)"))) %>%
-    addMarkers(
-      lng = data$lngm, lat = data$latm,
-      label = paste(sep = "\n", data$j, " (demand: ", data$demand, ")"),
-      labelOptions = labelOptions(closeButton = F, noHide = T, textsize = "22px", style= list("color" = "rgb(243, 150, 25)"))) %>%
+  map <- leaflet() %>%
+         addTiles() %>%
+      addMarkers(
+        lng = data$lngp, lat = data$latp,
+        label = paste0(data$i, " (capacity: ", data$cap, ")"),
+        labelOptions = labelOptions(noHide = T, textsize = "22px", style = list("background-color" = "rgb(243, 150, 25)"))) %>%
+      addMarkers(
+        lng = data$lngm, lat = data$latm,
+        label = paste(sep = "\n", data$j, " (demand: ", data$demand, ")"),
+        labelOptions = labelOptions(closeButton = F, noHide = T, textsize = "22px", style= list("color" = "rgb(243, 150, 25)"))) %>%
     
-    addFlows(lng0 = data$lngp, lat0 = data$latp, lng1 = data$lngm, lat1 = data$latm, 
-             color = "indianred", flow = data$quantities, opacity = 1, minThickness = 0, 
-             maxThickness = 12, 
-             layerId = paste0("From ", data$i, " to ", data$j), popup = popupArgs())
+         addFlows(lng0 = data$lngp, lat0 = data$latp, lng1 = data$lngm, lat1 = data$latm, 
+                  color = "indianred", flow = data$quantities, opacity = 1, minThickness = 0, 
+                  maxThickness = 12, 
+                  layerId = paste0("From ", data$i, " to ", data$j), popup = popupArgs())
   output$trnsport <- leaflet::renderLeaflet(map)
 }
