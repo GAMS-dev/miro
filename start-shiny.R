b044f4270d641ad14bbbbb2eeabc28e1ac12d1bb
Frederik Proske <fproske@gams.com>
2019-10-21 12:56:08 +0200

    - add start-shiny script to call as entry point

---
+++ start-shiny.R
@@ -0,0 +1,10 @@
+assign(".lib.loc", Sys.getenv("R_LIB_PATHS"), envir = environment(.libPaths))
+print(R.home())
+print(.libPaths())
+print(sessionInfo())
+suppressMessages(shiny::runApp(
+  Sys.getenv("RE_SHINY_PATH"),
+  host = "127.0.0.1",
+  launch.browser = identical(Sys.getenv("LAUNCHINBROWSER"), "true"),
+  port = as.integer(Sys.getenv("RE_SHINY_PORT"))
+))
