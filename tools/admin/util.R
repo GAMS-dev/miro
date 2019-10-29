70cd80237fc5f5c86675550106091e44b57d9c81
rschuchmann <rschuchmann@gams.com>
2019-10-29 16:28:41 +0100

    tool categories: graph configuration is now seperated in categories for each graph type only one dropdown for graph type selection fixed histogram renderer added orientation and cumulative options in histogram added type_convert in graph.R removed height from graph options re-design of graph configuration added arguments "class_outer" and "hr" to createArray function to change the style of an JS array many minor changes

--- tools/admin/util.R
+++ tools/admin/util.R
@@ -2,13 +2,14 @@ addArrayEl <- function(session, arrayID, plotlyChartType = "", defaults = NULL){
   arrayID <- paste0(arrayID, plotlyChartType)
   session$sendCustomMessage("gms-addArrayEl", list(arrayID = arrayID, defaults = defaults))
 }
-createArray <- function(session, arrayID, label, plotlyChartType = "", autoCreate = TRUE){
+createArray <- function(session, arrayID, label, plotlyChartType = "", autoCreate = TRUE, 
+                        class_outer = "array-wrapper-outer-default", hr = TRUE){
   if(autoCreate)
     addArrayEl(session, arrayID, plotlyChartType)
   arrayID <- paste0(arrayID, plotlyChartType)
-  HTML(paste0('<div id="', arrayID, '_wrapper" class="shiny-input-container" style="margin:20px;">\n
- <hr>\n
- <div class="array-wrapper"></div>\n
+  HTML(paste0('<div id="', arrayID, '_wrapper" class="shiny-input-container ', class_outer, '">\n', 
+              if ( hr ) "<hr>\n" else '',
+ '<div class="array-wrapper"></div>\n
    <div onclick="Miro.addArrayDataEl(\'', arrayID, '\')" style="cursor:pointer">\n
      <button type="button" class="btn btn-default bt-icon btn-add-array-el" style="font-size:20px;">\n
        <i class="far fa-plus-square"></i>\n
