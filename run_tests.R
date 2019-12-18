2f07bd3eeaf24324527eab505a2ff13e0f56c20d
Frederik Proske <fproske@gams.com>
2019-12-18 19:23:11 +0100

    - fixed some bugs that caused tests to fail

--- run_tests.R
+++ run_tests.R
@@ -3,5 +3,9 @@ library(testthat)
 options(testthat.output_file = "test-out.xml")
 options(testthat.default_reporter = "junit")
 
+RLibPath <- Sys.getenv("LIB_PATH")
+if(!identical(RLibPath, "")) {
+  .libPaths( c( RLibPath, .libPaths()) )
+}
 
 source(file.path("tests", "testthat.R"))
--- tests/testthat/test-ui.R
+++ tests/testthat/test-ui.R
@@ -4,10 +4,6 @@ library(shinytest)
 if(!dependenciesInstalled()){
   installDependencies()
 }
-RLibPath <- Sys.getenv("LIB_PATH")
-if(!identical(RLibPath, "")) {
-  .libPaths( c( RLibPath, .libPaths()) )
-}
 
 modelsToTest <- c("pickstock", "transport")
 testDir <- file.path(getwd(), "..")
@@ -25,14 +21,20 @@ testFiles <- c("excel_upload_test", "excel_upload_overwrite_test",
                "load_from_db_test", "gams_interrupt_test",
                "compare_scen_split_test", "compare_scen_tab_test")
 for(modelToTest in modelsToTest){
-  Sys.setenv(MIRO_MODEL_PATH = file.path(testDir, "..", "model", 
-                                         modelToTest, paste0(modelToTest, ".gms")))
+  testModelPath <- file.path(testDir, "..", "model", modelToTest)
+  Sys.setenv(MIRO_MODEL_PATH = file.path(testModelPath, 
+                                         paste0(modelToTest, ".gms")))
   Sys.setenv(GMSMODELNAME = modelToTest)
   
   for(testFile in testFiles){
     file.copy(paste0(testDir, .Platform$file.sep, testFile, ".R"), 
               paste0(testDir, .Platform$file.sep, testFile, "_", modelToTest, ".R"), overwrite = TRUE)
     
+  }
+  if(dir.exists(file.path(testModelPath, paste0("data_", modelToTest))) && 
+     unlink(file.path(testModelPath, paste0("data_", modelToTest)), 
+            recursive = TRUE, force = TRUE) != 0L){
+    warning(sprintf("Couldn't remove data dir of model: '%s'", modelToTest))
   }
   test_that(paste0("Uploading Excel file works for model: ", modelToTest),
     expect_pass(testApp(file.path(testDir, ".."), paste0("excel_upload_test_", modelToTest), 
