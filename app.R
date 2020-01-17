ba36774e035027bea6f884d6a91d634247858c58
Frederik Proske <fproske@gams.com>
2020-01-17 18:18:58 +0100

    - set default username for worker when noauth is used in shinyproxy

--- app.R
+++ app.R
@@ -704,7 +704,7 @@ if(is.null(errMsg)){
   credConfig <- NULL
   if(isShinyProxy){
     credConfig <- list(url = Sys.getenv("MIRO_GAMS_HOST"), 
-                       username = uid,
+                       username = if(identical(Sys.getenv("SHINYPROXY_NOAUTH"), "true")) "admin" else uid,
                        password = "",
                        namespace = "global",
                        useRegistered = TRUE)
