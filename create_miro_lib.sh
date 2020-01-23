07dccdabaaba9e59e347da05b710c8a9ad541bc1
Frederik Proske <fproske@gams.com>
2020-01-23 23:09:50 +0100

    - add dowjones2016.csv to unzip file

--- create_miro_lib.sh
+++ create_miro_lib.sh
@@ -5,7 +5,7 @@ cd transport
 zip transport.conf.unzip transport_files.txt conf_transport/transport.json -x "*.DS_Store"
 cd ..
 cd pickstock
-zip -r pickstock.conf.unzip pickstock_files.txt conf_pickstock/pickstock.json scripts_pickstock/hcube_analysis.ipynb static_pickstock -x "*.DS_Store"
+zip -r pickstock.conf.unzip pickstock_files.txt dowjones2016.csv conf_pickstock/pickstock.json scripts_pickstock/hcube_analysis.ipynb static_pickstock -x "*.DS_Store"
 cd ..
 cd sudoku
 zip -r sudoku.conf.unzip sudoku_files.txt conf_sudoku/sudoku.json renderer_sudoku/sudoku.R static_sudoku -x "*.DS_Store"
@@ -16,7 +16,7 @@ cp mirolib.glb miro_lib
 mv transport/transport.conf.unzip miro_lib
 cp transport/transport.gms miro_lib
 mv pickstock/pickstock.conf.unzip miro_lib
-cp pickstock/pickstock.gms pickstock/dowjones2016.csv miro_lib
+cp pickstock/pickstock.gms miro_lib
 mv sudoku/sudoku.conf.unzip miro_lib
 cp sudoku/sudoku.gms miro_lib
 zip -r miroDemoApps.zip miro_lib -x "*.DS_Store"
