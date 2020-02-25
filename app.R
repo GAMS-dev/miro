36319c72a7ddda7182148ae4a1d8adbef4414af6
Frederik Proske <fproske@gams.com>
2020-02-25 20:31:03 +0100

    - bump version number

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 #version number
-MIROVersion <- "0.9.86"
+MIROVersion <- "0.9.87"
 APIVersion  <- "1"
-MIRORDate   <- "Feb 21 2020"
+MIRORDate   <- "Feb 25 2020"
 #####packages:
 # processx        #MIT
 # dplyr           #MIT
