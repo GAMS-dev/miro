418610f4420af785533e0b856fcb76cfa1aabd14
Frederik Proske <fproske@gams.com>
2020-03-24 15:48:07 +0100

    - reverted appending path for script processes only and set it globally (for R session) instead as setting process wide does not work on Windows for some reason (probably issue with processx)

--- app.R
+++ app.R
@@ -65,6 +65,11 @@ requiredPackages <- c("jsonlite", "zip", "tibble", "readr")
 config <- list()
 gamsSysDir <- Sys.getenv("GAMS_SYS_DIR")
 
+if(length(gamsSysDir) && nchar(gamsSysDir) > 0L){
+  Sys.setenv(paste0(gamsSysDir, .Platform$path.sep, 
+                    Sys.getenv("PATH")))
+}
+
 installedPackages <<- installed.packages()[, "Package"]
 useGdx <<- FALSE
 if("gdxrrwMIRO" %in% installedPackages){
@@ -1293,14 +1298,12 @@ if(!is.null(errMsg)){
       if(LAUNCHHCUBEMODE){
         if(length(config$scripts)){
           scriptOutput <- ScriptOutput$new(session, file.path(workDir, paste0("scripts_", modelName)), 
-                                           config$scripts, lang$nav$scriptOutput$errMsg,
-                                           gamsSysDir)
+                                           config$scripts, lang$nav$scriptOutput$errMsg)
         }
       }else{
         if(length(config$scripts$base)){
           scriptOutput <- ScriptOutput$new(session, file.path(workDir, paste0("scripts_", modelName)),
-                                           config$scripts, lang$nav$scriptOutput$errMsg,
-                                           gamsSysDir)
+                                           config$scripts, lang$nav$scriptOutput$errMsg)
           observeEvent(input$runScript, {
             scriptId <- suppressWarnings(as.integer(input$runScript))
             if(is.na(scriptId) || scriptId < 1 || 
