ee1e96615a92ac6773259702762a97bbbd4c20ce
Frederik Proske <fproske@gams.com>
2020-03-31 01:00:02 +0200

    - no need to load data for inscribedsquare model

--- tests/solve_model_test.R
+++ tests/solve_model_test.R
@@ -1,6 +1,6 @@
 app <- ShinyDriver$new("../", loadTimeout = 10000)
 app$snapshotInit(paste0("solve_model_test_", Sys.getenv("GMSMODELNAME")))
-if(Sys.getenv("GMSMODELNAME") %in% c("inscribedsquare", "sudoku")){
+if(Sys.getenv("GMSMODELNAME") %in% c("sudoku")){
   app$setInputs(btImport = "click")
   Sys.sleep(1)
   app$setInputs(btLoadScenConfirm = "click")
