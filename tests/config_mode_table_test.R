e748de24d401cc7f6655f465584b132ee1e30208
Robin Schuchmann <rschuchmann@gams.com>
2020-07-15 10:31:22 +0200

    Add tests for symbol specific table settings

--- tests/config_mode_table_test.R
+++ tests/config_mode_table_test.R
@@ -37,5 +37,37 @@ expect_identical(configRaw$datatable$options$buttons, configNew$datatable$option
 expect_identical(configRaw$datatable$options$dom, configNew$datatable$options$dom)
 expect_identical(configRaw$datatable$extensions, configNew$datatable$extensions)
 
+#symbol table settings
+app$findElement("a[data-value='tables_gen']")$click()
+Sys.sleep(1)
+app$findElement("a[data-value='symbol']")$click()
+Sys.sleep(1)
+app$findElement("button[id='saveTableWidget']")$click()
+Sys.sleep(1)
+app$setInputs(table_symbol = "stock_weight")
+Sys.sleep(1)
+app$findElement("button[id='saveTableWidget']")$click()
+Sys.sleep(1)
+configNew <- suppressWarnings(jsonlite::fromJSON(file.path(jsonPath, "pickstock_configuration.json"),
+                                                 simplifyDataFrame = FALSE, 
+                                                 simplifyMatrix = FALSE))
+
+#input symbol price
+expect_identical(configRaw$inputWidgets$price$widgetType, configNew$inputWidgets$price$widgetType)
+expect_identical(configRaw$inputWidgets$price$bigData, configNew$inputWidgets$price$bigData)
+expect_identical(configRaw$inputWidgets$price$readonly, configNew$inputWidgets$price$readonly)
+expect_identical(configRaw$inputWidgets$price$readonlyCols, configNew$inputWidgets$price$readonlyCols)
+expect_identical(configRaw$inputWidgets$price$hideIndexCol, configNew$inputWidgets$price$hideIndexCol)
+expect_identical(configRaw$inputWidgets$price$heatmap, configNew$inputWidgets$price$heatmap)
+
+#output symbol stock_weight
+expect_identical(configRaw$outputTables$stock_weight$class, configNew$outputTables$stock_weight$class)
+expect_identical(configRaw$outputTables$stock_weight$filter, configNew$outputTables$stock_weight$filter)
+expect_identical(configRaw$outputTables$stock_weight$options$pageLength, configNew$outputTables$stock_weight$options$pageLength)
+expect_identical(configRaw$outputTables$stock_weight$options$decimals, configNew$outputTables$stock_weight$options$decimals)
+expect_identical(configRaw$outputTables$stock_weight$options$buttons, configNew$outputTables$stock_weight$options$buttons)
+expect_identical(configRaw$outputTables$stock_weight$options$dom, configNew$outputTables$stock_weight$options$dom)
+expect_identical(configRaw$outputTables$stock_weight$rownames, configNew$outputTables$stock_weight$rownames)
+expect_identical(configRaw$outputTables$stock_weight$extensions, configNew$outputTables$stock_weight$extensions)
 
 app$stop()
