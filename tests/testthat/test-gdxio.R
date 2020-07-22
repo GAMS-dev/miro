38fe3b3879efa304d8e7245167413be7a492b81e
Robin Schuchmann <rschuchmann@gams.com>
2020-07-22 13:47:41 +0200

    Modify path for gdxio tests to contain umlauts

--- tests/testthat/test-gdxio.R
+++ tests/testthat/test-gdxio.R
@@ -1,6 +1,6 @@
 context("gdxio class")
 
-load(file.path(getwd(), "data/test_gdxio.miroconf"), .GlobalEnv)
+load(file.path(getwd(), "dätä/test_gdxio.miroconf"), .GlobalEnv)
 source("../../global.R")
 source("../../components/gdxio.R")
 
@@ -11,52 +11,52 @@ gdxio <- GdxIO$new(file.path(.libPaths()[1], "gdxrrwMIRO", "bin"), c(modelInRaw,
                    list())
 
 test_that("Reading of set works", {
-  expect_equal(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "i"), 
+  expect_equal(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "i"), 
                tibble::tibble('1' = c("seattle", "san-diego"), '2' = rep.int(NA_character_, 2L)))
 })
 test_that("Reading of parameter works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "a"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "a"), 
                    tibble::tibble('1' = c("seattle", "san-diego"), '2' = c(350, 600)))
 })
 test_that("Reading of table with squeezed out column works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "squeezed_out"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "squeezed_out"), 
                    tibble::tibble('1' = c("seattle", "san-diego"), 'asd' = c(1, 1),
                                   'def' = c(NA_real_, NA_real_)))
   data <- list(tibble::tibble('1' = c("seattle", "san-diego"), 'asd' = c(0, 0),
                               'def' = c(1, 2)))
   names(data) <- "squeezed_out"
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/tests_gdxio.gdx"), "squeezed_out"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/tests_gdxio.gdx"), "squeezed_out"), 
                    tibble::tibble('1' = c("seattle", "san-diego"), 'asd' = c(0, 0),
                                   'def' = c(1, 2)))
 })
 test_that("Reading of (single) scalar works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "f"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "f"), 
                   90)
 })
 test_that("Reading of (single) singleton set works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "sub_i"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "sub_i"), 
                    tibble::tibble(`1` = "seattle", `2` = 'test'))
   ssData <- tibble::tibble(scalar = "sub_i", description = "test", value = "seattle")
   data <- list(ssData)
   names(data) <- scalarsFileName
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
-  expect_equal(gdxio$rgdx(file.path(getwd(), "data/tests_gdxio.gdx"), "sub_i"), 
+  expect_equal(gdxio$rgdx(file.path(getwd(), "dätä/tests_gdxio.gdx"), "sub_i"), 
                tibble::tibble(`1` = "seattle", `2` = NA_character_))
   unlink(filePath)
   ssData <- tibble::tibble(scalar = "sub_i", description = "test", value = "topeka||asd")
   data <- list(ssData)
   names(data) <- scalarsFileName
   gdxio$wgdx(filePath, data)
-  expect_equal(gdxio$rgdx(file.path(getwd(), "data/tests_gdxio.gdx"), "sub_i"), 
+  expect_equal(gdxio$rgdx(file.path(getwd(), "dätä/tests_gdxio.gdx"), "sub_i"), 
                tibble::tibble(`1` = "topeka", `2` = "asd"))
 })
 test_that("Reading of input scalars works", {
-  expect_equal(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), scalarsFileName), 
+  expect_equal(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), scalarsFileName), 
                    tibble::tibble(`scalarSymbols$symnames` = c('sub_i', 'f', 'mins', 'beta'),
                                   `scalarSymbols$symtext` = c('sub_i',
                                                               'freight in dollars per case per thousand miles',
@@ -65,21 +65,21 @@ test_that("Reading of input scalars works", {
                                   `vapply(...)` = c('seattle||test', 90, NA_character_, NA_character_)))
 })
 test_that("Reading of output scalars works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), scalarsOutName), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), scalarsOutName), 
                    tibble::tibble('scalarSymbols$symnames' = c('total_cost'), 
                           'scalarSymbols$symtext' = c('total transportation costs in thousands of dollars'),
                           'vapply(...)' = c(NA_character_)))
 })
 test_that("Reading of (single) scalar equation works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "cost"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "cost"), 
                    c(0, 1, 0, 0, 1))
 })
 test_that("Reading of (single) scalar variable works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), "z"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), "z"), 
                    c(153.675, 0, -Inf, Inf, 1))
 })
 test_that("Reading of scalar variables and equations works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), scalarEquationsOutName), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), scalarEquationsOutName), 
                    tibble::tibble('scalarSymbols$symnames[salarVeFound]' = c('z', 'cost'), 
                           'scalarSymbols$symtext[salarVeFound]' = c('total transportation costs in thousands of dollars',
                                                       'define objective function'),
@@ -87,12 +87,12 @@ test_that("Reading of scalar variables and equations works", {
                           '4' = c(Inf, 0), '5' = c(1, 1)))
 })
 test_that("Reading of equations works", {
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), 'supply'), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), 'supply'), 
                    tibble::tibble('1' = c("seattle", "san-diego"), l = c(350, 550), m = c(0, 0),
                           lo = c(-Inf, -Inf), up = c(350, 600), s = c(1, 1)))
 })
 test_that("Reading of variables works", {
-  expect_equal(as.data.frame(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), 'x')), 
+  expect_equal(as.data.frame(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), 'x')), 
                as.data.frame(tibble::tibble('1' = c("seattle", "seattle", "seattle",
                                             "san-diego", "san-diego", "san-diego"),
                                     '2' = c("new-york", "chicago", "topeka",
@@ -111,7 +111,7 @@ test_that("Writing of scalars works", {
                                `vapply(...)` = c('seattle||test', 60, NA_character_, NA_character_))
   data <- list(scalarData)
   names(data) <- scalarsFileName
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
   expect_equal(gdxio$rgdx(filePath, scalarsFileName), 
@@ -126,10 +126,10 @@ test_that("Writing of scalar variables and equations works", {
                              '4' = c(Inf, 0), '5' = c(1, 1))
   data <- list(scalarVe)
   names(data) <- scalarEquationsOutName
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/test_gdxio.gdx"), scalarEquationsOutName), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/test_gdxio.gdx"), scalarEquationsOutName), 
                    scalarVe)
 })
 
@@ -143,10 +143,10 @@ test_that("Writing of variables works", {
                             lo = rep.int(0, 6L), up = rep.int(Inf, 6L), s = rep.int(1, 6L))
   data <- list(varData)
   names(data) <- "x"
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
-  expect_equal(gdxio$rgdx(file.path(getwd(), "data/tests_gdxio.gdx"), 'x'), 
+  expect_equal(gdxio$rgdx(file.path(getwd(), "dätä/tests_gdxio.gdx"), 'x'), 
                varData)
 })
 
@@ -155,10 +155,10 @@ test_that("Writing of equations works", {
                            lo = c(-Inf, -Inf), up = c(350, 600), s = c(1, 1))
   data <- list(eqData)
   names(data) <- "supply"
-  filePath <- file.path(getwd(), "data/tests_gdxioeq.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxioeq.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/tests_gdxioeq.gdx"), "supply"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/tests_gdxioeq.gdx"), "supply"), 
                    eqData)
 })
 
@@ -176,7 +176,7 @@ test_that("Writing of singleton set with data from dropdown works", {
                                `vapply(...)` = 'seattle')
   data <- list(scalarData)
   names(data) <- scalarsFileName
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
   expect_equal(gdxio$rgdx(filePath, scalarsFileName), 
@@ -202,7 +202,7 @@ test_that("Writing of singleton set with data from dropdown and clearValue=TRUE 
                                `vapply(...)` = 'seattle')
   data <- list(scalarData)
   names(data) <- scalarsFileName
-  filePath <- file.path(getwd(), "data/tests_gdxio.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxio.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
   expect_equal(gdxio$rgdx(filePath, scalarsFileName), 
@@ -218,9 +218,9 @@ test_that("Reading/writing unicode characters work", {
   setData <- tibble::tibble('1' = c("seattle䤉䤉", "😀😀😀😀😀😀😀"), '2' = c("😈", "ધધધધ"))
   data <- list(setData)
   names(data) <- "i"
-  filePath <- file.path(getwd(), "data/tests_gdxiouni.gdx")
+  filePath <- file.path(getwd(), "dätä/tests_gdxiouni.gdx")
   on.exit(unlink(filePath), add = TRUE)
   gdxio$wgdx(filePath, data)
-  expect_identical(gdxio$rgdx(file.path(getwd(), "data/tests_gdxiouni.gdx"), "i"), 
+  expect_identical(gdxio$rgdx(file.path(getwd(), "dätä/tests_gdxiouni.gdx"), "i"), 
                    setData)
 })
