88996cdaf4e0b96e72f705e7037d98f5b0a1bd6c
Frederik Proske <fproske@gams.com>
2020-08-24 23:36:58 +0200

    Fix language tests

--- tests/lang_test.R
+++ tests/lang_test.R
@@ -1,6 +1,4 @@
 app <- ShinyDriver$new("../", loadTimeout = 20000)
 app$snapshotInit(paste0("lang_test_", Sys.getenv("MIRO_LANG")))
-Sys.sleep(1L)
-app$snapshot(items = list(input = paste0("slider_", c(2L, 2L + 1L))), 
-             screenshot = TRUE)
+app$snapshot(items = list(output = c("inputDataTitle")), screenshot = TRUE)
 app$stop()
