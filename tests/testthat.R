d3de09b0a5611101209e46bac80ba6ad386f36d8
Frederik Proske <fproske@gams.com>
2020-08-25 20:31:09 +0200

    Do more operations required by several tests in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,8 +2,15 @@ library("testthat")
 library("stringi")
 library("tibble")
 library("R6")
+library("shinytest")
 
+source(file.path("components", "global.R"))
 source(file.path("tests", "util.R"))
+source(file.path("components", "util.R"))
 
-#test_file("tests/testthat/test-util.R")
+if(!dependenciesInstalled()){
+  installDependencies()
+}
+
+#test_file("tests/testthat/test-gdxio-uni.R")
 test_dir("tests/testthat")
