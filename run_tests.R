0dd06f42ffc251a649c49bf8a80455ee6a985d5a
Frederik Proske <fproske@gams.com>
2020-09-23 18:57:19 +0200

    Remove setting of locale again

--- run_tests.R
+++ run_tests.R
@@ -6,10 +6,6 @@ if(!identical(RLibPath, "")) {
 options(testthat.output_file = "test-out.xml")
 options(testthat.default_reporter = "junit")
 
-if(.Platform$OS.type != 'windows'){
-  Sys.setlocale("LC_ALL","en_US.UTF-8")
-}
-
 print(sessionInfo())
 
 source(file.path("tests", "testthat.R"))
