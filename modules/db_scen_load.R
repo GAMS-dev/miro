GAMS-dev/miro

commit 8b77f32d5c9980de0e07ffe30394441d8d899245

Fix issue with load local tab in Hypercube mode compare mode
Fix a regression introduced in MIRO 1.2 that caused scenarios loaded in the tab view to not appear in the "local" tab in split view in Hypercube Mode

modules/db_scen_load.R
@@ -73,13 +73,13 @@ observeEvent(virtualActionButton(rv$btLoadScen), {
     sidsLoadedInOtherModes <- setdiff(c(sidsInComp, sidsInPivotComp), sidsInSplitComp)
     sidsLoadedInOtherModes <- sidsLoadedInOtherModes[sidsLoadedInOtherModes != 0]
     if(length(sidsLoadedInOtherModes)){
-      uiSidList <- db$formatScenList(scenMetaDb[scenMetaDb[[1]] %in% sidsLoadedInOtherModes, ],
-                                     stimeIdentifier, desc = TRUE)
+      uiSidListTmp <- sidsLoadedInOtherModes
     }
   }else if(is.null(input$btSplitView) && identical(config$defCompMode, "pivot") ||
       identical(input$btSplitView, "pivotView")){
     currentMode <- "pivot"
     sidsLoadedInPivotMode <- sidsInPivotComp[!is.na(sidsInPivotComp) & sidsInPivotComp != 0]
+    scenMetaDb <<- scenMetaDb[!as.character(scenMetaDb[[1]]) %in% sidsLoadedInPivotMode, ]
     if(length(sidsLoadedInPivotMode)){
       uiSidListTmp <- sidsLoadedInPivotMode
     }else{
@@ -120,9 +120,12 @@ observeEvent(virtualActionButton(rv$btLoadScen), {
   # by default, put most recently saved scenario first
   dbSidList <- db$formatScenList(scenMetaDbSubset, stimeIdentifier, desc = TRUE)
   if(!is.null(uiSidListTmp)){
-    uiSidList <- vapply(uiSidListTmp, function(i){
-      dbSidList[startsWith(dbSidList, paste0(i, "_"))][1]},
-      character(1L), USE.NAMES = FALSE)
+    sidListTmp <- bind_rows(scenMetaData[vapply(scenMetaData, function(el) !is.null(el[[1]]), 
+                                                logical(1L), USE.NAMES = FALSE)])
+    names(sidListTmp)[1:4] <- db$getScenMetaColnames()[c('sid', 'uid', 'sname', 'stime')]
+    uiSidList <- db$formatScenList(filter(sidListTmp, `_sid` %in% uiSidListTmp),
+                                   stimeIdentifier, desc = TRUE)
+    uiSidList <- uiSidList[!duplicated(uiSidList)]
   }
   showLoadScenDialog(dbSidList, uiSidList, isInSplitView, dbTagList = dbTagList)
   if(maxNoScenExceeded)

doc/release.html
@@ -67,6 +67,7 @@
                                     <h5 style="display:inline-block">GAMS MIRO 1.2.3</h5><div style="display:inline-block; padding-left:15px;"> (xxxx-xx-xx)</div>
                                     <ul>
                                       <li>Fix issue with arrays (e.g. y-data in a bar chart) in Configuration Mode sometimes not expanding.</li>
+                                       <li>Fix regression introduced with MIRO 1.2: In Hypercube Mode, scenarios loaded in the Tab View would not appear in the "Local" tab in the Split View.</li>
                                     </ul>
                                  </div>
                                  <div class="section-block">

commit 9a761df3a5d44b451ff631481b3ed29bc69e034e

Fix regression introduced with 8b77f32d
Adding scenarios to the Pivot Compare mode in base mode did not work anymore due to a bug introduced with 8b77f32d. We now handle base and Hypercube mode differently.

modules/db_scen_load.R
@@ -79,7 +79,6 @@ observeEvent(virtualActionButton(rv$btLoadScen), {
       identical(input$btSplitView, "pivotView")){
     currentMode <- "pivot"
     sidsLoadedInPivotMode <- sidsInPivotComp[!is.na(sidsInPivotComp) & sidsInPivotComp != 0]
-    scenMetaDb <<- scenMetaDb[!as.character(scenMetaDb[[1]]) %in% sidsLoadedInPivotMode, ]
     if(length(sidsLoadedInPivotMode)){
       uiSidListTmp <- sidsLoadedInPivotMode
     }else{
@@ -120,17 +119,26 @@ observeEvent(virtualActionButton(rv$btLoadScen), {
   # by default, put most recently saved scenario first
   dbSidList <- db$formatScenList(scenMetaDbSubset, stimeIdentifier, desc = TRUE)
   if(!is.null(uiSidListTmp)){
-    sidListTmp <- bind_rows(scenMetaData[vapply(scenMetaData, function(el) !is.null(el[[1]]), 
-                                                logical(1L), USE.NAMES = FALSE)])
-    names(sidListTmp)[1:4] <- db$getScenMetaColnames()[c('sid', 'uid', 'sname', 'stime')]
-    uiSidList <- db$formatScenList(filter(sidListTmp, `_sid` %in% uiSidListTmp),
-                                   stimeIdentifier, desc = TRUE)
     if(LAUNCHHCUBEMODE){
       # Hypercube scenarios are not part of dbSidList
+      sidListTmp <- bind_rows(scenMetaData[vapply(scenMetaData, function(el) !is.null(el[[1]]), 
+                                                  logical(1L), USE.NAMES = FALSE)])
+      if(length(sidListTmp)){
+        names(sidListTmp)[1:4] <- db$getScenMetaColnames()[c('sid', 'uid', 'sname', 'stime')]
+        uiSidList <- db$formatScenList(filter(sidListTmp, `_sid` %in% uiSidListTmp),
+                                       stimeIdentifier, desc = TRUE)
+      }else{
+        uiSidList <- NULL
+      }
       dbSidList <- c(dbSidList, uiSidList)
       dbSidList <- dbSidList[!duplicated(dbSidList)]
+    }else if(isInSplitView){
+      uiSidList <- db$formatScenList(scenMetaDb[scenMetaDb[[1]] %in% sidsLoadedInOtherModes, ],
+                                     stimeIdentifier, desc = TRUE)
     }else{
-      uiSidList <- uiSidList[!duplicated(uiSidList)]
+      uiSidList <- vapply(uiSidListTmp, function(i){
+        dbSidList[startsWith(dbSidList, paste0(i, "_"))][1]},
+        character(1L), USE.NAMES = FALSE)
     }
   }
   showLoadScenDialog(dbSidList, uiSidList, isInSplitView, dbTagList = dbTagList)