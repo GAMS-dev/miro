a0c53e54aee122cf5e9210f3c522961b6596bae1
Frederik Proske <fproske@gams.com>
2020-11-26 12:51:25 +0100

    Add UI test for comparison of sandbox scenario

---
+++ tests/testthat/test-sandbox-compare-ui.R
@@ -0,0 +1,33 @@
+context("UI tests - sandbox compare mode")
+
+testDir <- file.path(getwd(), "..")
+
+createTestDb()
+Sys.setenv(MIRO_DB_PATH = testDir)
+testModelPath <- file.path(testDir, "model", "transport")
+Sys.setenv(MIRO_MODEL_PATH = file.path(testModelPath,
+                                       "transport.gms"))
+Sys.setenv(MIRO_MODE="base")
+
+file.copy2(file.path(testModelPath, "conf_transport", "transport.json"),
+           file.path(testModelPath, "conf_transport", "transport_copy.json"))
+
+modelDataPath <- file.path(testModelPath, "data_transport")
+
+file.copy2(file.path(testDir, "data", "transport.gdx"),
+           file.path(modelDataPath, "default.gdx"))
+file.copy2(file.path(testDir, "data", "transport.gdx"),
+           file.path(modelDataPath, "default2.gdx"))
+file.copy2(file.path(testDir, "data", "transport.gdx"),
+           file.path(modelDataPath, "default3.gdx"))
+file.copy2(file.path(testDir, "data", "transport.gdx"),
+           file.path(modelDataPath, "default4.gdx"))
+test_that("Loading scenarios works",
+          expect_pass(testApp(file.path(testDir, ".."), "sandbox_compare_test",
+                              compareImages = FALSE)))
+unlink(modelDataPath, recursive = TRUE, force = TRUE)
+
+file.move(file.path(testModelPath, "conf_transport", "transport_copy.json"),
+          file.path(testModelPath, "conf_transport", "transport.json"))
+
+#Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "MIRO_MODE"))
