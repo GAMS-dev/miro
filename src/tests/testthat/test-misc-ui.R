8d91af157a7d5bb4b2514f9f6f6113fd9ba31736
Frederik Proske <fproske@gams.com>
2020-12-01 12:07:46 +0100

    Added MIRO_TEST_GAMS_LICE to wrong test

--- src/tests/testthat/test-misc-ui.R
+++ src/tests/testthat/test-misc-ui.R
@@ -1,6 +1,4 @@
 context("UI tests - Miscellaneous")
-skip_if(identical(Sys.getenv("GAMS_SYS_DIR"), ""),
-        "GAMS_SYS_DIR environment variable not set. Skipping tests.")
 
 testDir <- file.path(getwd(), "..")
 
@@ -19,18 +17,9 @@ file.copy2(file.path(testDir, "data", "transport.gdx"),
            file.path(modelDataPath, "default1.gdx"))
 file.copy2(file.path(testDir, "data", "transport2.gdx"),
            file.path(modelDataPath, "default2.gdx"))
-if(!identical(Sys.getenv("MIRO_TEST_GAMS_LICE"), "")){
-  saveAdditionalGamsClArgs(testModelPath, "transport",
-                           paste0('license="', Sys.getenv("MIRO_TEST_GAMS_LICE"), '"'))
-}
 test_that("Loading scenario while input graph view is active works",
           expect_pass(testApp(file.path(testDir, ".."), "load_input_graph_test",
                               compareImages = FALSE)))
 unlink(modelDataPath, recursive = TRUE, force = TRUE)
-
-if(!identical(Sys.getenv("MIRO_TEST_GAMS_LICE"), "")){
-  file.rename(file.path(testModelPath, "conf_transport", "transport_tmp.json"),
-              file.path(testModelPath, "conf_transport", "transport.json"))
-}
 
 Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "MIRO_MODE"))
