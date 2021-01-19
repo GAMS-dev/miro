999553e5fbca819868706db89967fd43e89b3564
Frederik Proske <fproske@gams.com>
2021-01-19 17:31:48 +0100

    Add missing library import to xlsio test

--- src/tests/testthat/test-xlsio-unit.R
+++ src/tests/testthat/test-xlsio-unit.R
@@ -1,5 +1,6 @@
 context("Unit tests - XlsIO class")
 library(readxl)
+library(writexl)
 library(dplyr)
 
 lang <<- jsonlite::fromJSON("../../conf/en.json", simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
