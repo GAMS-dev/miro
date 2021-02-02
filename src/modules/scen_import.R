GAMS-dev/miro

commit 15cceef12b1fd22a91b4b2d6794af8cf713b4437

Support reading hcube scalars from CSV files
Hcube scalars (scalars that are normal scalars in base mode, but are expanded in HC mode) can now be imported from CSV files

src/modules/scen_import.R
@@ -44,7 +44,9 @@ observe({
 output$csvHeaderMapping <- renderUI({
   req(rv$importCSV, input$selInputDataLocCSV)
   symSelected <- input$selInputDataLocCSV
-  if(!is.character(symSelected) || length(symSelected) != 1L || !symSelected %in% names(ioConfig$modelInRaw)){
+  if(!is.character(symSelected) ||
+     length(symSelected) != 1L ||
+     !symSelected %in% c(names(ioConfig$modelInRaw), ioConfig$hcubeScalars)){
     flog.error("selInputDataLocCSV has bad format. This is most likely because the user is trying to tamper with the app!")
     return(list())
   }
@@ -52,7 +54,11 @@ output$csvHeaderMapping <- renderUI({
     req(input$csvDelim)
     csvio$setRDelim(input$csvDelim)
   }
-  symHeaders <- ioConfig$modelInRaw[[symSelected]]$headers
+  if(symSelected %in% ioConfig$hcubeScalars){
+    symHeaders <- ioConfig$modelIn[[symSelected]]$headers
+  }else{
+    symHeaders <- ioConfig$modelInRaw[[symSelected]]$headers
+  }
   csvHeaders <- csvio$getHeaders()
   return(lapply(seq_along(symHeaders), function(hdrIdx){
     column(6L,
@@ -83,7 +89,7 @@ observeEvent(input$localInput, {
     hideEl(session, "#localInputCsvOptions")
     disableEl(session, "#btImportLocal")
     tryCatch({
-      xlsio$rInitFile(input$localInput$datapath)
+      xlsio$rInitFile(input$localInput$datapath, needDelim = FALSE)
       xlsSheets <- xlsio$getSheetNames()
       updateSelectInput(session, "selExcelIndexSheet", choices = c("-", xlsSheets),
                         selected = if("_index" %in% xlsSheets) "_index" else "-")
@@ -109,11 +115,12 @@ observeEvent(input$localInput, {
     }
     tryCatch({
       symId <- match(tolower(tools::file_path_sans_ext(input$localInput$name)),
-                     names(ioConfig$modelInRaw))
+                     c(names(ioConfig$modelInRaw), ioConfig$hcubeScalars))
       if(!is.na(symId)){
-        updateSelectInput(session, "selInputDataLocCSV", selected = names(ioConfig$modelInRaw)[symId])
+        updateSelectInput(session, "selInputDataLocCSV",
+                          selected = c(names(ioConfig$modelInRaw), ioConfig$hcubeScalars)[symId])
       }
-      csvio$rInitFile(input$localInput$datapath)
+      csvio$rInitFile(input$localInput$datapath, needDelim = FALSE)
       rv$importCSV <- rv$importCSV + 1L
       hideEl(session, "#csvDelimWrapper")
       enableEl(session, "#btImportLocal")
@@ -186,9 +193,15 @@ observeEvent(input$btImportLocal, {
         return()
       }
     }
-    colsToRead <- vapply(seq_along(ioConfig$modelInRaw[[symToFetch]]$headers), function(hdrIdx){
-      return(input[[paste0("csvInputHdr_", hdrIdx)]])
-    }, character(1L), USE.NAMES = FALSE)
+    if(symToFetch %in% ioConfig$hcubeScalars){
+      colsToRead <- vapply(seq_along(ioConfig$modelIn[[symToFetch]]$headers), function(hdrIdx){
+        return(input[[paste0("csvInputHdr_", hdrIdx)]])
+      }, character(1L), USE.NAMES = FALSE)
+    }else{
+      colsToRead <- vapply(seq_along(ioConfig$modelInRaw[[symToFetch]]$headers), function(hdrIdx){
+        return(input[[paste0("csvInputHdr_", hdrIdx)]])
+      }, character(1L), USE.NAMES = FALSE)
+    }
     if(any(duplicated(colsToRead[colsToRead != "-"]))){
       flog.info("Duplicate columns to read detected when uploading CSV file.")
       return(showElReplaceTxt(session, "#localDataImportError",

src/UI/dialogs.R
@@ -327,10 +327,10 @@ showLoadDataDialog <- function(scenListDb, dbTagList = NULL){
                                                     column(6,
                                                            selectInput("selInputDataLocCSV", lang$nav[[modeDescriptor]]$selInputData, 
                                                                        if(length(ioConfig$modelInRaw))
-                                                                         setNames(names(ioConfig$modelInRaw), 
+                                                                         c(setNames(names(ioConfig$modelInRaw), 
                                                                                   vapply(ioConfig$modelInRaw, function(inSym){
                                                                                     return(inSym$alias)
-                                                                                  }, character(1L), USE.NAMES = FALSE)) else NULL, 
+                                                                                  }, character(1L), USE.NAMES = FALSE)), if(length(ioConfig$hcubeScalars)) setNames(ioConfig$hcubeScalars, vapply(ioConfig$hcubeScalars, function(hcubeScalar){ return(ioConfig$modelIn[[hcubeScalar]]$alias)}, character(1L), USE.NAMES = FALSE))) else NULL, 
                                                                        multiple = FALSE, width = "100%")
                                                     ),
                                                     uiOutput("csvHeaderMapping")

src/components/csvio.R
@@ -1,5 +1,5 @@
 CsvIO <- R6::R6Class("CsvIO", inherit = LocalFileIO, public = list(
-  rInitFile = function(path, delim = NULL){
+  rInitFile = function(path, delim = NULL, needDelim = TRUE){
     private$rpath <- path
     private$rSymName <- character(0L)
     private$rHeaderRow <- TRUE
@@ -15,10 +15,14 @@ CsvIO <- R6::R6Class("CsvIO", inherit = LocalFileIO, public = list(
     if(is.null(delim)){
       private$rDelim <- private$guessDelim(private$rSample)
       if(length(private$rDelim) == 0L){
-        stop_custom("error_bad_delim",
-                    sprintf(lang$errMsg$csvio$errors$badDelim,
-                            paste(c(private$supportedDelim[private$supportedDelim != "\t"], "tab"),
-                                  collapse = "' '")), call. = FALSE)
+        if(needDelim){
+          stop_custom("error_bad_delim",
+                      sprintf(lang$errMsg$csvio$errors$badDelim,
+                              paste(c(private$supportedDelim[private$supportedDelim != "\t"], "tab"),
+                                    collapse = "' '")), call. = FALSE)
+        }else{
+          private$rDelim <- private$supportedDelim[1]
+        }
       }
       if(length(private$rDelim) > 1L){
         stop_custom("error_ambiguous_delim",

commit 049347d557fb68bc5faa4576800e269d4952f91b

Fix typo introduced in 15cceef1 (bad argument to xlsio)

src/modules/scen_import.R
@@ -89,7 +89,7 @@ observeEvent(input$localInput, {
     hideEl(session, "#localInputCsvOptions")
     disableEl(session, "#btImportLocal")
     tryCatch({
-      xlsio$rInitFile(input$localInput$datapath, needDelim = FALSE)
+      xlsio$rInitFile(input$localInput$datapath)
       xlsSheets <- xlsio$getSheetNames()
       updateSelectInput(session, "selExcelIndexSheet", choices = c("-", xlsSheets),
                         selected = if("_index" %in% xlsSheets) "_index" else "-")