2dbae5ee609c82777670df963ccb6e0dc3035951
Frederik Proske <fproske@gams.com>
2021-04-06 19:39:21 +0200

    Fix table settings UI test

--- src/tests/testthat/test-tables-ui.R
+++ src/tests/testthat/test-tables-ui.R
@@ -1,7 +1,6 @@
 context("UI tests - Table settings")
 
 createTestDb()
-# END setup
 
 Sys.setenv(MIRO_MODEL_PATH = file.path(getwd(), "..", "model", "pickstock_output_tables",
                                        "pickstock_output_tables.gms"))
@@ -22,6 +21,7 @@ Sys.setenv(MIRO_MODEL_PATH = file.path(getwd(), "..", "model", "transport",
                                        "transport.gms"))
 file.copy2(file.path("..", "model", "transport", "conf_transport", "transport.json"),
            file.path("..", "model", "transport", "conf_transport", "bk_transport.json"))
+createTestDb()
 test_that("Input table dropdownCols work",
           expect_pass(testApp(file.path(testDir, ".."), "input_table_dropdowncols_test",
                               compareImages = FALSE)))
