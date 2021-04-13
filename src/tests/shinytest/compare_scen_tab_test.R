4e31f67c4e78080141283a7279f3385920407868
Frederik Proske <fproske@gams.com>
2021-04-13 10:13:22 +0200

    UI tests: Fix some comparison mode tests

--- src/tests/shinytest/compare_scen_tab_test.R
+++ src/tests/shinytest/compare_scen_tab_test.R
@@ -7,10 +7,19 @@ app$findElements(".btSplitView a[data-view='tab']")[[1]]$click()
 Sys.sleep(0.5)
 app$findElement("#cmpTabNoScenWrapper .action-button")$click()
 Sys.sleep(1)
-app$setInputs(selLoadScen = paste0(c("1_", "2_"), Sys.info()[["user"]]), wait_ = FALSE, values_ = FALSE)
+scenOptions <- setNames(getSelectizeAliases(app, "#selLoadScen"),
+                        getSelectizeOptions(app, "#selLoadScen"))
+scenToCompare <- paste0(c("1_", "2_"), Sys.info()[["user"]])
+app$setInputs(selLoadScen = scenToCompare, wait_ = FALSE, values_ = FALSE)
 app$setInputs(btLoadScenConfirm = "click")
 Sys.sleep(2)
-app$snapshot(items = list(output = c("cmpScenTitle_4", "cmpScenTitle_5")), screenshot = TRUE)
+expect_true(app$waitFor(paste0("$('#cmpScenTitle_4').text()==='",
+                               strsplit(scenOptions[scenToCompare[1]], " (", fixed = TRUE)[[1]][1],
+                               "';"), timeout = 50))
+expect_true(app$waitFor(paste0("$('#cmpScenTitle_5').text()==='",
+                               strsplit(scenOptions[scenToCompare[2]], " (", fixed = TRUE)[[1]][1],
+                               "';"), timeout = 50))
+app$snapshot(items = list(output = c("inputDataTitle")), screenshot = TRUE)
 app$findElement("#btScenTableView5")$click()
 
 app$stop()
