4da54dd09c5e7c7dfa570057ed6542e0ffac73b9
Frederik Proske <fproske@gams.com>
2021-04-25 19:46:41 +0200

    Doc: Another attempt at offsetting anchors

--- doc/assets/js/main.js
+++ doc/assets/js/main.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
     });
 
     /* Activate scrollspy menu */
-    $('body').scrollspy({target: '#doc-menu', offset: 100});
+    $('body').scrollspy({target: '#doc-menu', offset: -100});
     
     /* Smooth scrolling */
 	$('a.scrollto').on('click', function(e){
         //store hash
         var target = this.hash;    
         e.preventDefault();
-		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
+		$('body').scrollTo(target, 800, {offset: -60.3, 'axis':'y'});
 		
 	});
 	
