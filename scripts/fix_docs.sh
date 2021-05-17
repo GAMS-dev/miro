eff290a464971799d3a1078dbe2e8e3217e77299
Frederik Proske <fproske@gams.com>
2021-05-17 18:25:19 +0200

    Update MIRO Server license template during build

--- scripts/fix_docs.sh
+++ scripts/fix_docs.sh
@@ -16,6 +16,7 @@ MIRO_RELEASE_DATE=$(grep -m 1 -e 'MIRORDate' src/app.R | cut -f2 -d'"' | xargs -
 echo $MIRO_VERSION_FULL | sed 's/\./,/g'> ./doc/latest.ver
 sed -e '/<code class="language-json">/r./src/conf/config_schema.json' ./doc/schema_template.html >./doc/schema.html
 sed -e '/<pre id="miro-license">/r./src/LICENSE' ./doc/license_template.html >./doc/license.html
+sed -e '/<pre id="miro-license">/r./server/LICENSE' ./doc/license_template_server.html >./doc/license_server.html
 sed -i -e "s/\.\/download\.html\">Get GAMS MIRO .*<\/a>/\.\/download\.html\">Get GAMS MIRO $MIRO_VERSION_FULL<\/a>/g" ./doc/index.html
 sed -e 's/__VERSION__/'${MIRO_VERSION_SHORT}'/g' ./doc/download_template.html | sed -e 's/__VERSION_FULL__/'${MIRO_VERSION_FULL}'/g'>./doc/download.html
 sed -i -e "s/(xxxx-xx-xx)/($MIRO_RELEASE_DATE)/" ./doc/release.html
