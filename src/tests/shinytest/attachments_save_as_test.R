88d826bbc3c380a32b6e9f8b9bc2785018229da8
Frederik Proske <fproske@gams.com>
2021-06-08 12:12:11 +0200

    Fix some UI tests due tabset ID change

--- src/tests/shinytest/attachments_save_as_test.R
+++ src/tests/shinytest/attachments_save_as_test.R
@@ -39,7 +39,7 @@ Sys.sleep(1)
 #get user name
 app$setInputs(btEditMeta = "click")
 Sys.sleep(0.5)
-app$findElement("a[data-value='Access permissions']")$click()
+app$findElement("a[data-value='accessPerm']")$click()
 Sys.sleep(0.1)
 user <-app$getValue("editMetaWritePerm")[[1]]
 app$setInputs(btUpdateMeta = "click")
