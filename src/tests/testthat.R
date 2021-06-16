81052cec999c6fa736fe85e1c00117d9441ed94c
Frederik Proske <fproske@gams.com>
2021-06-16 12:52:59 +0200

    Set log file path when running tests

--- src/tests/testthat.R
+++ src/tests/testthat.R
@@ -10,6 +10,9 @@ print("Home directory:")
 print(path.expand("~"))
 print("Working directory:")
 print(getwd())
+logPathTests <- file.path(getwd(), "tests", "logs-tests")
+print("Log file directory:")
+print(logPathTests)
 
 library("testthat")
 library("stringi")
@@ -26,6 +29,19 @@ if(!dependenciesInstalled()){
   installDependencies()
 }
 
+if(dir.exists(logPathTests) && !identical(unlink(logPathTests, recursive = TRUE), 0L)){
+  stop(sprintf("Can't remove existing log file directory: '%s'. Do you lack write permissions?",
+               logPathTests),
+       call. = FALSE)
+}
+if(!dir.create(logPathTests)){
+  stop(sprintf("Can't create log file directory: '%s'. Do you lack write permissions?",
+               logPathTests),
+       call. = FALSE)
+}
+
+Sys.setenv(MIRO_LOG_PATH = logPathTests)
+
 reporter <- MultiReporter$new(list(
   ProgressReporter$new(max_failures = 100),
   JunitReporter$new(file = file.path(getwd(), "test-out.xml"))
@@ -34,5 +50,7 @@ reporter <- MultiReporter$new(list(
 stopOnFailure <- identical(commandArgs(trailingOnly=TRUE), "--stop")
 testDir <- file.path(getwd(), "tests")
 
-#test_file("tests/testthat/test-load-compare-scen-ui.R", reporter = reporter)
+#test_file("tests/testthat/test-attachments-ui.R", reporter = reporter)
 test_dir("tests/testthat", reporter = reporter, stop_on_failure = stopOnFailure)
+
+Sys.unsetenv("MIRO_LOG_PATH")
