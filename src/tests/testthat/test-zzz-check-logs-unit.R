ef3e1ce089ffa0b2f540088d2961f3e06f356e1d
Frederik Proske <fproske@gams.com>
2021-06-16 17:38:43 +0200

    Allow defining exceptions when parsing logs for errors

--- src/tests/testthat/test-zzz-check-logs-unit.R
+++ src/tests/testthat/test-zzz-check-logs-unit.R
@@ -1,21 +1,77 @@
 context("Unit tests - check log files")
 
+validFatals <- c("GAMS symbol: 'd': Invalid column name(s): 'bla' in configuration for dropdown columns",
+                 "The column: 'i' of the GAMS symbol: 'd': cannot be declared both as a pivot column and a drop-down column")
+validErrors <- c("Confirm download of temporary files button clicked without any files specified.")
+validWarnings <- c()
+
 logFilePath <- Sys.getenv("MIRO_LOG_PATH")
 
 logFiles <- list.files(logFilePath, pattern = ".log$", full.names = TRUE)
 
 for(logFile in logFiles){
   logContent <- readr::read_lines(logFile)
-  errorsInLog <- startsWith(logContent, "ERROR") | startsWith(logContent, "FATAL")
+  fatalsInLog <- startsWith(logContent, "FATAL")
+  errorMsg <- strsplit(logContent[fatalsInLog], "] ", fixed = TRUE)
+  errorMsg <- vapply(seq_along(errorMsg), function(lineIdx){
+    if(length(errorMsg[[lineIdx]]) > 1L){
+      return(errorMsg[[lineIdx]][2])
+    }
+    return(logContent[[which(fatalsInLog)[lineIdx] + 1L]])
+  }, FUN.VALUE = character(1L), USE.NAMES = FALSE)
+  for(i in seq_along(validFatals)){
+    falsePositives <- startsWith(errorMsg, validFatals[i])
+    errorMsg <- errorMsg[!falsePositives]
+    if(any(falsePositives)){
+      validFatals <- validFatals[-i]
+    }
+  }
+  test_that(sprintf("Log file: %s contains no fatal errors.", basename(logFile)),
+            expect_false(length(errorMsg) > 0L))
+  if(length(errorMsg)){
+    print(sprintf("FATALS in: %s", basename(logFile)))
+    print(errorMsg)
+  }
+  errorsInLog <- startsWith(logContent, "ERROR")
+  errorMsg <- strsplit(logContent[errorsInLog], "] ", fixed = TRUE)
+  errorMsg <- vapply(seq_along(errorMsg), function(lineIdx){
+    if(length(errorMsg[[lineIdx]]) > 1L){
+      return(errorMsg[[lineIdx]][2])
+    }
+    return(logContent[[which(errorsInLog)[lineIdx] + 1L]])
+  }, FUN.VALUE = character(1L), USE.NAMES = FALSE)
+  for(i in seq_along(validErrors)){
+    falsePositives <- startsWith(errorMsg, validErrors[i])
+    errorMsg <- errorMsg[!falsePositives]
+    if(any(falsePositives)){
+      validErrors <- validErrors[-i]
+    }
+  }
   test_that(sprintf("Log file: %s contains no errors.", basename(logFile)),
-            expect_false(any(errorsInLog)))
-  if(any(errorsInLog)){
-    print(logContent[errorsInLog])
+            expect_false(length(errorMsg) > 0L))
+  if(length(errorMsg)){
+    print(sprintf("ERRORS in: %s", basename(logFile)))
+    print(errorMsg)
   }
   warningsInLog <- startsWith(logContent, "WARN")
+  errorMsg <- strsplit(logContent[warningsInLog], "] ", fixed = TRUE)
+  errorMsg <- vapply(seq_along(errorMsg), function(lineIdx){
+    if(length(errorMsg[[lineIdx]]) > 1L){
+      return(errorMsg[[lineIdx]][2])
+    }
+    return(logContent[[which(warningsInLog)[lineIdx] + 1L]])
+  }, FUN.VALUE = character(1L), USE.NAMES = FALSE)
+  for(i in seq_along(validWarnings)){
+    falsePositives <- startsWith(errorMsg, validWarnings[i])
+    errorMsg <- errorMsg[!falsePositives]
+    if(any(falsePositives)){
+      validWarnings <- validWarnings[-i]
+    }
+  }
   test_that(sprintf("Log file: %s contains no warnings.", basename(logFile)),
-            expect_false(any(warningsInLog)))
-  if(any(warningsInLog)){
-    print(logContent[warningsInLog])
+            expect_false(length(errorMsg) > 0L))
+  if(length(errorMsg)){
+    print(sprintf("WARNINGS in: %s", basename(logFile)))
+    print(errorMsg)
   }
 }
