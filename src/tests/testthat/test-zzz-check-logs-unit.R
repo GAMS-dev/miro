8e4961006e0a96b9e2c0013eb1e50df4f1d2b1fc
Frederik Proske <fproske@gams.com>
2021-06-16 14:07:23 +0200

    Fix test logs test

--- src/tests/testthat/test-zzz-check-logs-unit.R
+++ src/tests/testthat/test-zzz-check-logs-unit.R
@@ -6,13 +6,13 @@ logFiles <- list.files(logFilePath, pattern = ".log$", full.names = TRUE)
 
 for(logFile in logFiles){
   logContent <- readr::read_lines(logFile)
-  errorsInLog <- startsWith("ERROR", logContent) | startsWith("FATAL", logContent)
+  errorsInLog <- startsWith(logContent, "ERROR") | startsWith(logContent, "FATAL")
   test_that(sprintf("Log file: %s contains no errors.", basename(logFile)),
             expect_false(any(errorsInLog)))
   if(any(errorsInLog)){
     print(logContent[errorsInLog])
   }
-  warningsInLog <- startsWith("WARN", logContent)
+  warningsInLog <- startsWith(logContent, "WARN")
   test_that(sprintf("Log file: %s contains no warnings.", basename(logFile)),
             expect_false(any(warningsInLog)))
   if(any(warningsInLog)){
