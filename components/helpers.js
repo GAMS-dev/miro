0137e3d3bba29b355bc93af72d894ec21858d17a
Frederik Proske <fproske@gams.com>
2021-08-05 11:25:50 +0200

    [Launcher] Update list of forbidden ports

--- components/helpers.js
+++ components/helpers.js
@@ -29,8 +29,8 @@ const randomInt = (min, max) => Math.round(Math.random() * ((max + 1) - min) + m
 const randomPort = async () => {
   /* eslint-disable no-constant-condition, no-await-in-loop, no-continue */
   // Those forbidden ports are in line with shiny
-  // https://github.com/rstudio/shiny/blob/288039162086e183a89523ac0aacab824ef7f016/R/server.R#L734
-  const forbiddenPorts = [3659, 4045, 6000, 6665, 6666, 6667, 6668, 6669, 6697];
+  // https://github.com/rstudio/shiny/blob/29b574bf94c56e69e621be8fd6d3b7eb0ae42a18/R/runapp.R#L308
+  const forbiddenPorts = [3659, 4045, 5060, 5061, 6000, 6566, 6665, 6666, 6667, 6668, 6669, 6697];
   while (true) {
     let port = randomInt(3000, 5000);
     if (forbiddenPorts.includes(port)) continue;
