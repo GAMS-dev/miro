d5684b97b34c95c237ec6274a78f61e39c4c3911
Frederik Proske <fproske@gams.com>
2021-08-09 15:17:56 +0200

    [Engine backend] Added status code -403: Forbidden

--- src/global.R
+++ src/global.R
@@ -125,6 +125,7 @@ JOBSTATUSMAP <- c(
 GAMSRCMAP <- c(
   '-500' = "Internal error",
   '-404' = "Host could not be reached",
+  '-403' = "Forbidden",
   '-401' = "Access denied",
   '-400' = "License expired",
   '-100' = "Model execution timed out",
