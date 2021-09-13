676b3d081f1a6a3a0cd544b6a36be4fa560d9526
Frederik Proske <fproske@gams.com>
2021-09-13 17:50:27 +0200

    [Improve mobile] Allow custom apple-touch-icon

--- src/UI/body.R
+++ src/UI/body.R
@@ -929,7 +929,11 @@ if (buildUI) {
         } else {
           tags$link(rel = "manifest", href = "manifest.json")
         },
-        tags$link(rel = "apple-touch-icon", sizes = "180x180", href = "apple-touch-icon.png"),
+        if (staticDirExists && file.exists(file.path(currentModelDir, paste0("static_", modelName), "apple-touch-icon.png"))) {
+          tags$link(rel = "apple-touch-icon", sizes = "180x180", href = paste0("static_", modelName, "/apple-touch-icon.png"))
+        } else {
+          tags$link(rel = "apple-touch-icon", sizes = "180x180", href = "apple-touch-icon.png")
+        },
         tags$link(rel = "icon", type = "image/png", sizes = "32x32", href = "favicon-32x32.png"),
         tags$link(rel = "icon", type = "image/png", sizes = "16x16", href = "favicon-16x16.png"),
         tags$meta(name = "msapplication-TileColor", content = "#ff9900"),
