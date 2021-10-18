3260db4c40ac8f4f17ad11ff9790b89ef56f585b
Frederik Proske <fproske@gams.com>
2021-10-18 18:22:38 +0200

    [Meta] Update release date

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const installRPackages = require('./components/install-r');
 
 const requiredAPIVersion = 1;
 const miroVersion = '2.1.3';
-const miroRelease = 'Oct 14 2021';
+const miroRelease = 'Oct 18 2021';
 const libVersion = '2.1';
 const exampleAppsData = require('./components/example-apps')(miroVersion, requiredAPIVersion);
 const LangParser = require('./components/LangParser');
