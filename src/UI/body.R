9d5fa5da5fa12cc7f0861c26beb41259d3a6bf0f
Frederik Proske <fproske@gams.com>
2021-12-08 15:04:42 +0100

    [Server] Fix PWA metadata on Apple devices

--- src/UI/body.R
+++ src/UI/body.R
@@ -973,6 +973,11 @@ if (buildUI) {
       )
     )
   ))
+  if (isShinyProxy) {
+    staticDir <- "../../assets/static/"
+  } else {
+    staticDir <- ""
+  }
   miroBody <- dashboardBody({
     tagList(
       tags$head(
@@ -1004,18 +1009,18 @@ if (buildUI) {
         if (staticDirExists && file.exists(file.path(currentModelDir, paste0("static_", modelName), "apple-touch-icon.png"))) {
           tags$link(rel = "apple-touch-icon", sizes = "180x180", href = paste0("static_", modelName, "/apple-touch-icon.png"))
         } else {
-          tags$link(rel = "apple-touch-icon", sizes = "180x180", href = "apple-touch-icon.png")
+          tags$link(rel = "apple-touch-icon", sizes = "180x180", href = paste0(staticDir, "apple-touch-icon.png"))
         },
-        tags$link(href = "iphone5_splash.png", media = "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
-        tags$link(href = "iphone6_splash.png", media = "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
-        tags$link(href = "iphoneplus_splash.png", media = "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)", rel = "apple-touch-startup-image"),
-        tags$link(href = "iphonex_splash.png", media = "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", rel = "apple-touch-startup-image"),
-        tags$link(href = "iphonexr_splash.png", media = "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
-        tags$link(href = "iphonexsmax_splash.png", media = "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)", rel = "apple-touch-startup-image"),
-        tags$link(href = "ipad_splash.png", media = "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
-        tags$link(href = "ipadpro1_splash.png", media = "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
-        tags$link(href = "ipadpro3_splash.png", media = "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
-        tags$link(href = "ipadpro2_splash.png", media = "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "iphone5_splash.png"), media = "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "iphone6_splash.png"), media = "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "iphoneplus_splash.png"), media = "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "iphonex_splash.png"), media = "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "iphonexr_splash.png"), media = "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "iphonexsmax_splash.png"), media = "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "ipad_splash.png"), media = "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "ipadpro1_splash.png"), media = "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "ipadpro3_splash.png"), media = "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
+        tags$link(href = paste0(staticDir, "ipadpro2_splash.png"), media = "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)", rel = "apple-touch-startup-image"),
         tags$link(rel = "icon", type = "image/png", sizes = "32x32", href = "favicon-32x32.png"),
         tags$link(rel = "icon", type = "image/png", sizes = "16x16", href = "favicon-16x16.png"),
         tags$meta(name = "msapplication-TileColor", content = "#ff9900"),
