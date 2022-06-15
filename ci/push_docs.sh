61975418bdb7cb5035b97c7a8e5930d948159b78
Frederik Proske <fproske@gams.com>
2022-06-15 13:40:48 +0200

    [CI] Migrate build and deploy job on Linux

---
+++ ci/push_docs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+MIRO_VERSION_FULL=$(grep -e '"version": .*' package.json |cut -f4 -d"\"")
+pushd src > /dev/null
+    MIRO_RELEASE_DATE=$(grep -m 1 -e 'MIRORDate' app.R | cut -f2 -d'"' | xargs -0 date +%Y-%m-%d -d )
+popd > /dev/null
+./scripts/fix_docs.sh
+
+# update MIRO Server API OpenAPI schema
+pushd server > /dev/null
+    cp -R release_data/data_raw/ data
+    mkdir models
+    python3 miro_server.py dump_schema ../doc/miro_server_api.json
+    rm -rf data models
+popd > /dev/null
+
+git add ./doc/release.html ./doc/index.html ./doc/miro_server_api.json
+
+git commit -m "[CI skip] Update documentation" || true
+
+git remote rm origin && git remote add origin "https://${GITLAB_USERNAME}:${GITLAB_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
+
+git tag -fa "v${MIRO_VERSION_FULL}" -m "version ${MIRO_VERSION_FULL}"
+
+git push origin "HEAD:$CI_COMMIT_REF_NAME"
+git push origin "v${MIRO_VERSION_FULL}" || {
+    git push origin ":refs/tags/v${MIRO_VERSION_FULL}"
+    git push origin "v${MIRO_VERSION_FULL}"
+}
+
+rsync -rlptvz doc/* ubuntu@new.gams.com:/var/www/html/new.gams.com/public_html/miro
---
+++ ci/update_website.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+GAMSCOMDIR=update_website
+
+[ -d "$GAMSCOMDIR" ] || {
+  git clone https://${GITLAB_USERNAME}:${GITLAB_TOKEN}@git.gams.com/smann/new.gams.com.git --branch master --single-branch "$GAMSCOMDIR"
+}
+
+MIRO_VERSION_FULL=$(grep -e '"version": .*' package.json |cut -f4 -d"\"")
+pushd src > /dev/null
+    MIRO_RELEASE_DATE=$(grep -m 1 -e 'MIRORDate' app.R | cut -f2 -d'"' | xargs -0 date +%Y-%m-%d -d )
+popd > /dev/null
+
+pushd "$GAMSCOMDIR" > /dev/null
+  git pull
+  sed -i -e 's/^\\( *miro_release *= *\\).*/\\1"'"${MIRO_VERSION_FULL}"'"/' ./config.toml
+  sed -i -e 's/^\\( *miro_release_date *= *\\).*/\\1"'"${MIRO_RELEASE_DATE}"'"/' ./config.toml
+  git add config.toml
+  git commit -m "Update MIRO version" && {
+      git push origin HEAD:master || true
+  }
+popd > /dev/null
