9478de01d29d410654cd00ef4f35b42665896102
Frederik Proske <fproske@gams.com>
2022-06-15 15:26:26 +0200

    [CI] Add pre-commit hook for updating version

---
+++ scripts/fixVersion.R
@@ -0,0 +1,52 @@
+# replace MIRO API version, MIRO version and MIRO release date in main.js and package.json with the one set in src/app.R
+eval(parse(text = readLines("./src/app.R",
+  n = 5L, warn = FALSE
+)))
+mainJS <- readLines("./main.js", warn = FALSE)
+mainJS <- gsub(
+  "const requiredAPIVersion = \\d+;",
+  paste0("const requiredAPIVersion = ", APIVersion, ";"), mainJS
+)
+mainJS <- gsub(
+  "const miroVersion = '[^']+';",
+  paste0("const miroVersion = '", MIROVersion, "';"), mainJS
+)
+mainJS <- gsub(
+  "const miroRelease = '[^']+';",
+  paste0("const miroRelease = '", MIRORDate, "';"), mainJS
+)
+writeLines(mainJS, "./main.js")
+packageJSON <- readLines("./package.json", warn = FALSE)
+packageJSON <- gsub(
+  '"version": "[^"]+",',
+  paste0('"version": "', MIROVersion, '",'), packageJSON
+)
+writeLines(packageJSON, "./package.json")
+adminConfig <- readLines("./server/admin/global.R", warn = FALSE)
+adminConfig <- gsub(
+  'MIRO_VERSION[[:space:]]*<-[[:space:]]*"[^"]+"',
+  paste0('MIRO_VERSION <- "', MIROVersion, '"'), adminConfig
+)
+adminConfig <- gsub(
+  "REQUIRED_API_VERSION[[:space:]]*<-.*",
+  paste0("REQUIRED_API_VERSION <- ", APIVersion), adminConfig
+)
+writeLines(adminConfig, "./server/admin/global.R")
+dockerImageMiro <- readLines("./server/ui/Dockerfile", warn = FALSE)
+dockerImageMiro <- gsub(
+  'com\\.gamsmiro\\.version="[^"]+"',
+  paste0('com.gamsmiro.version="', MIROVersion, '"'), dockerImageMiro
+)
+writeLines(dockerImageMiro, "./server/ui/Dockerfile")
+dockerImageAdmin <- readLines("./server/admin/Dockerfile", warn = FALSE)
+dockerImageAdmin <- gsub(
+  'com\\.gamsmiroadmin\\.version="[^"]+"',
+  paste0('com.gamsmiroadmin.version="', MIROVersion, '"'), dockerImageAdmin
+)
+writeLines(dockerImageAdmin, "./server/admin/Dockerfile")
+dockerImageAuth <- readLines("./server/auth/Dockerfile", warn = FALSE)
+dockerImageAuth <- gsub(
+  'com\\.gamsmiroauth\\.version="[^"]+"',
+  paste0('com.gamsmiroauth.version="', MIROVersion, '"'), dockerImageAuth
+)
+writeLines(dockerImageAuth, "./server/auth/Dockerfile")
