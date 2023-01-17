a397f70414586a16f32015987cfd7fcf5abe5ab2
Frederik Proske <fproske@gams.com>
2023-01-17 19:42:47 +0100

    [CI] No longer require shinytest package

--- build/scripts/install-packages.R
+++ build/scripts/install-packages.R
@@ -75,7 +75,7 @@ newPackages <- requiredPackages[!requiredPackages %in%
   installedPackagesDevel[, "Package"]]
 
 # make sure correct version of packages is installed
-devPkgVersionMap <- list(list("shinytest", c(1, 4)), list("zip", c(2, 1)))
+devPkgVersionMap <- list(list("zip", c(2, 1)))
 for (devPkgToInstall in devPkgVersionMap) {
   if (!devPkgToInstall[[1]] %in% newPackages) {
     pkgId <- match(devPkgToInstall[[1]], installedPackagesDevel[, "Package"])
