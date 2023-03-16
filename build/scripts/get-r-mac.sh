342e020570ad0f056ddcca499d33dc060fbdc967
Frederik Proske <fproske@gams.com>
2023-03-16 20:48:12 +0100

    [CI] Download proper R version for ARM

--- build/scripts/get-r-mac.sh
+++ build/scripts/get-r-mac.sh
@@ -5,7 +5,11 @@ set -e
 # Requires xar and cpio
 rm -rf r
 mkdir -p r
-curl -o r/r.pkg https://cloud.r-project.org/bin/macosx/base/R-${R_BASE_VERSION}.pkg
+if [[ $(uname -p) == 'arm' ]]; then
+    curl -o r/r.pkg https://cloud.r-project.org/bin/macosx/big-sur-arm64/base/R-${R_BASE_VERSION}-arm64.pkg
+else
+    curl -o r/r.pkg https://cloud.r-project.org/bin/macosx/base/R-${R_BASE_VERSION}.pkg
+fi
 
 pushd r > /dev/null
     xar -xf r.pkg
