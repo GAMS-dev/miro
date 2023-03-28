GAMS-dev/miro

commit ba169f69b705c2983ab85095e62ea685f555c547

[Log] Always write stdout to file
Regardless of `storeLogFilesDuration` setting or whether attachments
module is activated, we now always write the log file to disk.

src/modules/gams_run.R
@@ -502,16 +502,6 @@ if (config$activateModules$logFile ||
   } else {
     logFilePath <- file.path(workDir, config$miroLogFile)
   }
-  writeLogFileToDisk <- TRUE
-  if (config$activateModules$attachments &&
-    config$storeLogFilesDuration > 0L &&
-    !is.null(activeScen)) {
-    if (!config$activateModules$logFile) {
-      writeLogFileToDisk <- FALSE
-    }
-  } else {
-    writeLogFileToDisk <- FALSE
-  }
 
   logObs <- observe({
     req(rv$triggerAsyncProcObserver)
@@ -523,9 +513,7 @@ if (config$activateModules$logFile ||
       return()
     }
 
-    if (writeLogFileToDisk) {
-      write_file(logText, logFilePath, append = TRUE)
-    }
+    write_file(logText, logFilePath, append = TRUE)
     appendEl(session, "#logStatusContainer", logText,
       scroll = identical(isolate(input$logUpdate), TRUE)
     )

commit 9479088307392078532d47f98cd3df925149ae52

[MIRO log] Fix duplicated log content
Regression was introduced with ba169f69b705c2983ab85095e62ea685f555c547:
we should not write MIRO log to disk as it is already on disk..

doc/release.html
@@ -125,6 +125,11 @@
                         MIRO Server: Fix issue where the custom page title was
                         not applied on the login page.
                       </li>
+                      <li>
+                        Fix regression introduced with MIRO 2.6.0 where the
+                        content of the MIRO log file was duplicated multiple
+                        times when the (normal) log file was disabled.
+                      </li>
                     </ul>
                   </div>
                 </section>

src/modules/gams_run.R
@@ -516,7 +516,9 @@ if (config$activateModules$logFile ||
       return()
     }
 
-    write_file(logText, logFilePath, append = TRUE)
+    if (config$activateModules$logFile) {
+      write_file(logText, logFilePath, append = TRUE)
+    }
     appendEl(session, "#logStatusContainer", logText,
       scroll = identical(isolate(input$logUpdate), TRUE)
     )

src/tests/model/sudoku_custom_widget/sudoku.gms
@@ -62,6 +62,10 @@ file log / miro.log /;
 put log '------------------------------------'/;
 put log '        Data validation'/;
 put log '------------------------------------'/;
+if(test123 < 0,
+    put log 'initial_state:: test123 must be positive!'/;
+    abort "Data errors detected."
+);
 if(card(error01),
   put log 'initial_state_clean:: Digits must be integers between 0 and 9!'/;
   loop(error01(row,col),

src/tests/shinytest/mirolog_test_custom_widget.R
@@ -1,0 +1,15 @@
+app <- AppDriver$new("../../", name = "mirolog_test_custom_widget", variant = NULL, load_timeout = 20000)
+Sys.sleep(1)
+app$set_inputs(btImport = "click")
+app$wait_for_js("($('#shiny-modal').data('bs.modal')||{}).isShown===true", timeout = 10000L)
+Sys.sleep(0.1)
+app$set_inputs(tb_importData = "tb_importData_local", wait_ = FALSE)
+Sys.sleep(0.5)
+app$upload_file(localInput = "../data/sudoku_mirolog_fail.gdx")
+app$set_inputs(btImportLocal = "click")
+app$wait_for_js("($('#shiny-modal').data('bs.modal')||{}).isShown!==true", timeout = 10000L)
+Sys.sleep(0.1)
+app$set_inputs(btSolve = "click")
+expect_error(app$wait_for_js("$('#valErr_initial_state').is(':visible');", timeout = 10000L), NA)
+expect_error(app$wait_for_js("$('#valErr_initial_state').text()===' test123 must be positive!';", timeout = 500L), NA)
+app$stop()

src/tests/testthat/test-mirolog-ui.R
@@ -30,4 +30,40 @@ if (length(additionalGamsClArgs)) {
     file.path(miroModelDir, "conf_pickstock", "pickstock.json")
   )
 }
+
+createTestDb()
+
+miroModelDir <- file.path(testDir, "model", "sudoku_custom_widget")
+Sys.setenv(MIRO_MODEL_PATH = file.path(miroModelDir, "sudoku.gms"))
+Sys.setenv(GMSMODELNAME = "sudoku")
+# deactivate log file (only MIRO log activated)
+configJSONFileName <- file.path(miroModelDir, "conf_sudoku", "sudoku.json")
+file.copy(configJSONFileName,
+  file.path(dirname(configJSONFileName), "sudoku_tmp.json"),
+  overwrite = TRUE
+)
+configJSON <- suppressWarnings(jsonlite::fromJSON(configJSONFileName,
+  simplifyDataFrame = FALSE,
+  simplifyMatrix = FALSE
+))
+configJSON$activateModules$logFile <- FALSE
+jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
+if (!identical(Sys.getenv("MIRO_TEST_GAMS_LICE"), "")) {
+  saveAdditionalGamsClArgs(
+    miroModelDir, "sudoku",
+    paste0('license="', Sys.getenv("MIRO_TEST_GAMS_LICE"), '"')
+  )
+}
+test_that(
+  "MIRO log works with custom input widgets",
+  {
+    source(file.path(testDir, "shinytest", "mirolog_test_custom_widget.R"), local = TRUE)
+  }
+)
+
+file.rename(
+  file.path(miroModelDir, "conf_sudoku", "sudoku_tmp.json"),
+  file.path(miroModelDir, "conf_sudoku", "sudoku.json")
+)
+
 Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "GMSMODELNAME"))