e09f1068e150f99b352d1de6f7b4c368f8a247aa
Frederik Proske <fproske@gams.com>
2023-10-25 15:55:49 +0200

    [Gallery] Remove outdated validateApp function

--- doc/gallery/app/routes.py
+++ doc/gallery/app/routes.py
@@ -28,36 +28,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in {"miroapp"}
 
-def validateApp(data):
-    zip_data = zipfile.ZipFile(io.BytesIO(data.read()), "r")
-    files_in_bundle = zip_data.namelist()
-    miroconf_format= re.compile('(.*)_(\d)_(\d+)_(\d+\.\d+\.\d+)(_hcube)?\.miroconf$')
-    miroconf_files = filter(miroconf_format.match, files_in_bundle)
-
-    modes_supported = []
-    model_name = None
-
-    for miroconf_file in miroconf_files:
-        miroconf = miroconf_format.search(miroconf_file)
-        with zip_data.open(miroconf_file, 'r') as miroconf_data:
-            if miroconf_data.read(2) != b'\x1f\x8b':
-                raise ValidationError(f'Invalid app bundle uploaded.')
-        if not model_name:
-            model_name = miroconf.group(1)
-            if miroconf.group(2) != '1':
-                raise ValidationError('Invalid app bundle uploaded. Model was not deployed for multi user environment!')
-            api_version = miroconf.group(3)
-            miro_version = miroconf.group(4)
-            if api_version != REQUIRED_API_VERSION:
-                raise ValidationError('MIRO app outdated: Please deploy your MIRO application with the latest MIRO version!')
-        if miroconf.group(5):
-            modes_supported.append('hcube')
-        else:
-            modes_supported.append('base')
-    if not len(modes_supported) or 'base' not in modes_supported:
-        raise ValidationError('Please make sure you deployed your app for the Base Mode.')
-    return model_name.lower()
-
 @app.route("/api/vote-up", methods=["POST"])
 def voteup():
     logging.info(f"New upvote request received for app id: {request.form['id']}")
@@ -91,15 +61,6 @@ def add_app():
         logging.error(f"Invalid file uploaded ({miro_app_filename}). Looks like an attempt to tamper with the app.")
         return jsonify({"status": 1, "message": "An unexpected error occurred"}), 200
 
-    try:
-        model_name = validateApp(miro_app_file)
-    except ValidationError as ve:
-        logging.info(f"Validation error. Error message: {str(ve)}")
-        return jsonify({"status": 1, "message": str(ve)}), 200
-    except Exception as e:
-        logging.error(f"Problems validating new app. Error message: {str(e)}")
-        return jsonify({"status": 1, "message": "An unexpected error occurred"}), 200
-
     app_title = request.form["app-title"]
     if not app_title:
         logging.error(f"No app title was specified. Looks like an attempt to tamper with the app.")
@@ -121,7 +82,7 @@ def add_app():
         logging.info(f"No valid E-mail specified: {author_mail}.")
         return jsonify({"status": 1, "message":"E-Mail entered is invalid. Please enter a valid E-mail address."}), 200
 
-    new_app_dir_name = os.path.join("data", "user_apps", secure_filename(author_name.lower() + "_" + model_name))
+    new_app_dir_name = os.path.join("data", "user_apps", secure_filename(author_name.lower() + "_" + app_title))
     try:
         i = 1
         while os.path.isdir(new_app_dir_name):
@@ -138,7 +99,7 @@ def add_app():
         miro_app_file.save(miro_app_filepath)
 
         with open(os.path.join(new_app_dir_name, "config.txt"), "w") as config_file:
-            config_file.write(f"Id: {model_name}\ntitle: {app_title}\ndesc: ${app_desc}\n\
+            config_file.write(f"Title: {app_title}\ntitle: {app_title}\ndesc: ${app_desc}\n\
 author: {author_name}\nemail: {author_mail}")
     except Exception as e:
         logging.error(f"Problems saving miro app to directory: {new_app_dir_name}. Error message: {str(e)}")
@@ -148,7 +109,7 @@ author: {author_name}\nemail: {author_mail}")
         msg = Message(f"New MIRO app",
                   sender="noreply@gams.com",
                   recipients=["miro@gams.com"])
-        msg.body = f"New MIRO app: {model_name} from: {author_name} ({author_mail}) uploaded. App description: {app_desc}"
+        msg.body = f"New MIRO app: {app_title} from: {author_name} ({author_mail}) uploaded. App description: {app_desc}"
         with app.open_resource(os.path.join("..", miro_app_filepath)) as fp:
             msg.attach(secure_filename(miro_app_filename), "application/zip", fp.read())
         mail.send(msg)
