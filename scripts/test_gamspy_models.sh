c1d65bb5af0ebb9421090aef56a25b7b88fd7e17
Frederik Proske <fproske@gams.com>
2024-02-20 18:45:57 +0100

    [CI] Do not ignore pickstock GAMSPy example

--- scripts/test_gamspy_models.sh
+++ scripts/test_gamspy_models.sh
@@ -8,9 +8,6 @@ pushd src/model/gamspy > /dev/null
     for dir in */
     do
         dir=${dir%*/}
-        if [[ "$dir" == 'pickstock' ]]; then
-            continue
-        fi
         cd "$dir"
             mv "conf_${dir}/${dir}_io.json" "conf_${dir}/${dir}_io_test.json"
             MIRO=1 python "$dir.py"
