b9b11f5ab6ba3eece20ca1bfffec40b8d18c715d
Frederik Proske <fproske@gams.com>
2024-03-01 18:19:06 +0100

    [Server] Re-add CSP

--- server/proxy/templates/2col/assets/js/app.js
+++ server/proxy/templates/2col/assets/js/app.js
@@ -1,24 +1,52 @@
-$(function() {
-    const currLocation = window.location.pathname;
-    if (!(currLocation.endsWith("app/admin") || currLocation.endsWith("app/admin/"))) {
-      let timer;
-      $("#navbarHandle").on('mouseover', function () {
-        clearTimeout(timer);
-        $("#navbarWrapper").show();
-      });
-      $("#navbarWrapper").on('mouseover', function () {
-        clearTimeout(timer);
-        $("#navbarWrapper").show();
-      });
-      $("#navbarWrapper").on('mouseleave', function () {
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-          $("#navbarWrapper").hide();
-        }, 600);
-      });
-    }
-
-    $('.btn-restart-app').on('click', () => {
-      window.location.reload();
+$(function () {
+  const currLocation = window.location.pathname;
+  if (!(currLocation.endsWith("app/admin") || currLocation.endsWith("app/admin/"))) {
+    let timer;
+    $("#navbarHandle").on('mouseover', function () {
+      clearTimeout(timer);
+      $("#navbarWrapper").show();
     });
+    $("#navbarWrapper").on('mouseover', function () {
+      clearTimeout(timer);
+      $("#navbarWrapper").show();
+    });
+    $("#navbarWrapper").on('mouseleave', function () {
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        $("#navbarWrapper").hide();
+      }, 600);
+    });
+  } else {
+    $('#navAdminPanel').addClass('active').attr('href', '#');
+    $("#navbarWrapper").show().attr('href', '#');
+  }
+
+  $('.btn-restart-app').on('click', () => {
+    window.location.reload();
+  });
+
+  const appConfig = document.getElementById('appConfig')?.dataset;
+  if (appConfig) {
+    window.Shiny.common.init(
+      appConfig.contextPath??null,
+      appConfig.application_name??null,
+      appConfig.spInstance??null,
+      appConfig.appMaxInstances??null,
+      appConfig.myAppsMode??null,
+      appConfig.pauseSupported==='true');
+    window.Shiny.app.start(
+      appConfig.proxy??null,
+      appConfig.heartbeatRate == null? 10000: parseInt(appConfig.heartbeatRate),
+      appConfig.appName??null,
+      appConfig.appInstance??null,
+      appConfig.parameterAllowedCombinations??null,
+      appConfig.parameterDefinitions??null,
+      appConfig.parameterIds??null,
+      appConfig.appPath??"",
+      appConfig.containerSubPath??"",
+    );
+    if (appConfig.refreshOpenidEnabled==='true') {
+      window.Shiny.connections.startOpenidRefresh();
+    }
+  }
 });
