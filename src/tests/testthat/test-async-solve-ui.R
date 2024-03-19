b960048d495f8d9c3ce158f2c5e185ad4f41da1c
Frederik Proske <fproske@gams.com>
2024-03-19 17:10:23 +0100

    [Tests] Retry async tests

--- src/tests/testthat/test-async-solve-ui.R
+++ src/tests/testthat/test-async-solve-ui.R
@@ -1,136 +1,130 @@
-context("UI tests - asynchronous solve")
-skip_if(
-  identical(Sys.getenv("ENGINE_URL"), ""),
-  "Skipping asynchronous solve tests as ENGINE_URL was not set."
-)
-skip_if(
-  identical(Sys.getenv("ENGINE_USER"), ""),
-  "Skipping asynchronous solve tests as ENGINE_USER was not set."
-)
-skip_if(
-  identical(Sys.getenv("ENGINE_PASSWORD"), ""),
-  "Skipping asynchronous solve tests as ENGINE_PASSWORD was not set."
-)
-skip_if(
-  identical(Sys.getenv("ENGINE_NS"), ""),
-  "Skipping asynchronous solve tests as ENGINE_NS was not set."
-)
-
-createTestDb()
-
-apiURL <- Sys.getenv("ENGINE_URL")
-inviterUser <- Sys.getenv("ENGINE_USER")
-inviterPass <- Sys.getenv("ENGINE_PASSWORD")
-namespace <- Sys.getenv("ENGINE_NS")
-
-inviteeName <- paste0(Sys.getenv("ENGINE_USER"), "_", round(runif(1, 1, 100000)))
-
-createUser(apiURL, inviterUser, inviterPass, namespace,
-  inviteeName, inviterPass,
-  volumeQuota = 160L
-)
-
-Sys.setenv("MIRO_REMOTE_EXEC_URL" = apiURL)
-Sys.setenv("MIRO_REMOTE_EXEC_USERNAME" = inviteeName)
-Sys.setenv("MIRO_REMOTE_EXEC_TOKEN" = getEngineToken(apiURL, inviteeName, inviterPass))
-Sys.setenv("MIRO_REMOTE_EXEC_NS" = namespace)
-
-modelToTest <- "transport"
-modelToTestUpper <- paste0(toupper(substr(modelToTest, 1, 1)), substring(modelToTest, 2))
-testModelDir <- file.path(testDir, "model", modelToTest)
-modelDataPath <- file.path(testModelDir, paste0("data_", modelToTest))
-configJSONFileName <- file.path(
-  testModelDir, paste0("conf_", modelToTest),
-  paste0(modelToTest, ".json")
-)
-
-file.move(
-  file.path(testModelDir, paste0(modelToTest, ".gms")),
-  file.path(testModelDir, paste0("bk_", modelToTest, ".gms"))
-)
-file.move(
-  file.path(testModelDir, paste0(modelToTest, "_files.txt")),
-  file.path(testModelDir, paste0("bk_", modelToTest, "_files.txt"))
-)
-file.copy(
-  file.path(testModelDir, paste0("bk_", modelToTest, ".gms")),
-  file.path(testModelDir, paste0(modelToTestUpper, ".gms"))
-)
-modelToTest <- modelToTestUpper
-Sys.setenv(MIRO_MODEL_PATH = file.path(testModelDir, paste0(modelToTest, ".gms")))
-Sys.setenv(MIRO_MODE = "base")
-Sys.setenv(MIRO_REMOTE_EXEC = "true")
-
-writeLines(
-  paste0(modelToTest, ".gms"),
-  file.path(testModelDir, paste0(tolower(modelToTest), "_files.txt"))
-)
-
-# add --sleep=1 to extraClArgs and hide log and lst file
-file.copy(configJSONFileName, file.path(
-  dirname(configJSONFileName),
-  paste0(tolower(modelToTest), "_tmp.json")
-), overwrite = TRUE)
-configJSON <- suppressWarnings(jsonlite::fromJSON(configJSONFileName,
-  simplifyDataFrame = FALSE,
-  simplifyMatrix = FALSE
-))
-configJSON$extraClArgs <- c(configJSON$extraClArgs, "--sleep=1", "--largeLog=1")
-configJSON$activateModules$logFile <- FALSE
-configJSON$activateModules$lstFile <- FALSE
-jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
-file.copy2(file.path(testDir, "data", "transport.gdx"), file.path(modelDataPath, "default.gdx"))
-
-if (file.exists(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))))) {
-  unlink(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))), force = TRUE)
-}
-
 test_that(
-  "Solve asynchronously with GAMS Engine works",
-  {
+  "Asynchronous solve works",
+  try_again(as.integer(Sys.getenv("MIRO_MAX_RETRIES", "3")), {
+    skip_if(
+      identical(Sys.getenv("ENGINE_URL"), ""),
+      "Skipping asynchronous solve tests as ENGINE_URL was not set."
+    )
+    skip_if(
+      identical(Sys.getenv("ENGINE_USER"), ""),
+      "Skipping asynchronous solve tests as ENGINE_USER was not set."
+    )
+    skip_if(
+      identical(Sys.getenv("ENGINE_PASSWORD"), ""),
+      "Skipping asynchronous solve tests as ENGINE_PASSWORD was not set."
+    )
+    skip_if(
+      identical(Sys.getenv("ENGINE_NS"), ""),
+      "Skipping asynchronous solve tests as ENGINE_NS was not set."
+    )
+
+    createTestDb()
+
+    apiURL <- Sys.getenv("ENGINE_URL")
+    inviterUser <- Sys.getenv("ENGINE_USER")
+    inviterPass <- Sys.getenv("ENGINE_PASSWORD")
+    namespace <- Sys.getenv("ENGINE_NS")
+
+    inviteeName <- paste0(Sys.getenv("ENGINE_USER"), "_", round(runif(1, 1, 100000)))
+
+    createUser(apiURL, inviterUser, inviterPass, namespace,
+      inviteeName, inviterPass,
+      volumeQuota = 160L
+    )
+
+    Sys.setenv("MIRO_REMOTE_EXEC_URL" = apiURL)
+    Sys.setenv("MIRO_REMOTE_EXEC_USERNAME" = inviteeName)
+    Sys.setenv("MIRO_REMOTE_EXEC_TOKEN" = getEngineToken(apiURL, inviteeName, inviterPass))
+    Sys.setenv("MIRO_REMOTE_EXEC_NS" = namespace)
+
+    modelToTest <- "transport"
+    modelToTestUpper <- paste0(toupper(substr(modelToTest, 1, 1)), substring(modelToTest, 2))
+    testModelDir <- file.path(testDir, "model", modelToTest)
+    modelDataPath <- file.path(testModelDir, paste0("data_", modelToTest))
+    configJSONFileName <- file.path(
+      testModelDir, paste0("conf_", modelToTest),
+      paste0(modelToTest, ".json")
+    )
+
+    file.move(
+      file.path(testModelDir, paste0(modelToTest, ".gms")),
+      file.path(testModelDir, paste0("bk_", modelToTest, ".gms"))
+    )
+    file.move(
+      file.path(testModelDir, paste0(modelToTest, "_files.txt")),
+      file.path(testModelDir, paste0("bk_", modelToTest, "_files.txt"))
+    )
+    file.copy(
+      file.path(testModelDir, paste0("bk_", modelToTest, ".gms")),
+      file.path(testModelDir, paste0(modelToTestUpper, ".gms"))
+    )
+    modelToTest <- modelToTestUpper
+    Sys.setenv(MIRO_MODEL_PATH = file.path(testModelDir, paste0(modelToTest, ".gms")))
+    Sys.setenv(MIRO_MODE = "base")
+    Sys.setenv(MIRO_REMOTE_EXEC = "true")
+
+    writeLines(
+      paste0(modelToTest, ".gms"),
+      file.path(testModelDir, paste0(tolower(modelToTest), "_files.txt"))
+    )
+
+    # add --sleep=1 to extraClArgs and hide log and lst file
+    file.copy(configJSONFileName, file.path(
+      dirname(configJSONFileName),
+      paste0(tolower(modelToTest), "_tmp.json")
+    ), overwrite = TRUE)
+    configJSON <- suppressWarnings(jsonlite::fromJSON(configJSONFileName,
+      simplifyDataFrame = FALSE,
+      simplifyMatrix = FALSE
+    ))
+    configJSON$extraClArgs <- c(configJSON$extraClArgs, "--sleep=1", "--largeLog=1")
+    configJSON$activateModules$logFile <- FALSE
+    configJSON$activateModules$lstFile <- FALSE
+    jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
+    file.copy2(file.path(testDir, "data", "transport.gdx"), file.path(modelDataPath, "default.gdx"))
+
+    if (file.exists(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))))) {
+      unlink(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))), force = TRUE)
+    }
+
     source(file.path(testDir, "shinytest", "async_solve_test.R"), local = TRUE)
-  }
-)
 
-removeUser(apiURL, inviterUser, inviterPass, inviteeName)
+    removeUser(apiURL, inviterUser, inviterPass, inviteeName)
 
-createUser(
-  apiURL, inviterUser, inviterPass, namespace,
-  inviteeName, inviterPass
-)
+    createUser(
+      apiURL, inviterUser, inviterPass, namespace,
+      inviteeName, inviterPass
+    )
 
-Sys.setenv("MIRO_REMOTE_EXEC_TOKEN" = getEngineToken(apiURL, inviteeName, inviterPass))
+    Sys.setenv("MIRO_REMOTE_EXEC_TOKEN" = getEngineToken(apiURL, inviteeName, inviterPass))
 
-configJSON$extraClArgs <- c(configJSON$extraClArgs, "--sleep=30")
-configJSON$activateModules$logFile <- TRUE
-jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
+    configJSON$extraClArgs <- c(configJSON$extraClArgs, "--sleep=30")
+    configJSON$activateModules$logFile <- TRUE
+    jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
 
-test_that(
-  "Detaching from active solve works",
-  {
     source(file.path(testDir, "shinytest", "async_solve_detach_test.R"), local = TRUE)
-  }
-)
 
-removeUser(apiURL, inviterUser, inviterPass, inviteeName)
+    removeUser(apiURL, inviterUser, inviterPass, inviteeName)
 
-file.rename(
-  file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), "_tmp.json")),
-  file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), ".json"))
-)
+    file.rename(
+      file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), "_tmp.json")),
+      file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), ".json"))
+    )
 
-unlink(file.path(testModelDir, paste0(modelToTestUpper, ".gms")),
-  recursive = TRUE, force = FALSE
-)
-file.move(
-  file.path(testModelDir, paste0("bk_", tolower(modelToTest), ".gms")),
-  file.path(testModelDir, paste0(tolower(modelToTest), ".gms"))
-)
-file.move(
-  file.path(testModelDir, paste0("bk_", tolower(modelToTest), "_files.txt")),
-  file.path(testModelDir, paste0(tolower(modelToTest), "_files.txt"))
-)
+    unlink(file.path(testModelDir, paste0(modelToTestUpper, ".gms")),
+      recursive = TRUE, force = FALSE
+    )
+    file.move(
+      file.path(testModelDir, paste0("bk_", tolower(modelToTest), ".gms")),
+      file.path(testModelDir, paste0(tolower(modelToTest), ".gms"))
+    )
+    file.move(
+      file.path(testModelDir, paste0("bk_", tolower(modelToTest), "_files.txt")),
+      file.path(testModelDir, paste0(tolower(modelToTest), "_files.txt"))
+    )
 
-unlink(modelDataPath, recursive = TRUE, force = TRUE)
+    unlink(modelDataPath, recursive = TRUE, force = TRUE)
 
-Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "MIRO_MODE", "MIRO_REMOTE_EXEC"))
+    Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "MIRO_MODE", "MIRO_REMOTE_EXEC"))
+  })
+)
--- src/tests/testthat/test-hcube-module-ui.R
+++ src/tests/testthat/test-hcube-module-ui.R
@@ -1,138 +1,130 @@
-context("UI tests - Hypercube Module")
-skip_if(
-  identical(Sys.getenv("ENGINE_URL"), ""),
-  "Skipping asynchronous solve tests as no ENGINE_URL was not set."
-)
-skip_if(
-  identical(Sys.getenv("ENGINE_USER"), ""),
-  "Skipping asynchronous solve tests as no ENGINE_USER was not set."
-)
-skip_if(
-  identical(Sys.getenv("ENGINE_PASSWORD"), ""),
-  "Skipping asynchronous solve tests as no ENGINE_PASSWORD was not set."
-)
-skip_if(
-  identical(Sys.getenv("ENGINE_NS"), ""),
-  "Skipping asynchronous solve tests as no ENGINE_NS was not set."
-)
-
-createTestDb()
-
-apiURL <- Sys.getenv("ENGINE_URL")
-inviterUser <- Sys.getenv("ENGINE_USER")
-inviterPass <- Sys.getenv("ENGINE_PASSWORD")
-namespace <- Sys.getenv("ENGINE_NS")
-
-inviteeName <- paste0(Sys.getenv("ENGINE_USER"), "_", round(runif(1, 1, 100000)))
-
-createUser(apiURL, inviterUser, inviterPass, namespace,
-  inviteeName, inviterPass,
-  volumeQuota = 68L
-)
-
-modelToTest <- "pickstock_configuration"
-testModelDir <- file.path(testDir, "model", modelToTest)
-modelDataPath <- file.path(testModelDir, paste0("data_", modelToTest))
-configJSONFileName <- file.path(
-  testModelDir, paste0("conf_", modelToTest),
-  paste0(modelToTest, ".json")
-)
-# END setup
-
-Sys.setenv(MIRO_MODEL_PATH = file.path(testModelDir, paste0(modelToTest, ".gms")))
-Sys.setenv(MIRO_MODEL_NAME = modelToTest)
-Sys.setenv(MIRO_REMOTE_EXEC = "true")
-Sys.setenv("MIRO_REMOTE_EXEC_URL" = apiURL)
-Sys.setenv("MIRO_REMOTE_EXEC_USERNAME" = inviteeName)
-Sys.setenv("MIRO_REMOTE_EXEC_TOKEN" = getEngineToken(apiURL, inviteeName, inviterPass))
-Sys.setenv("MIRO_REMOTE_EXEC_NS" = namespace)
-
-# activate local upload module, deactivate
-file.copy(file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), "_expected.json")),
-  file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), ".json")),
-  overwrite = TRUE
-)
-configJSON <- suppressWarnings(jsonlite::fromJSON(configJSONFileName,
-  simplifyDataFrame = FALSE,
-  simplifyMatrix = FALSE
-))
-configJSON$activateModules$loadLocal <- TRUE
-configJSON$activateModules$hcube <- TRUE
-configJSON$activateModules$attachments <- TRUE
-configJSON$extraClArgs <- c(configJSON$extraClArgs, "--sleep=1")
-configJSON$defCompMode <- "pivot"
-
-configJSON$inputWidgets[["_gmspar_sliderrange"]]$noHcube <- FALSE
-configJSON$inputWidgets[["trainingdays"]]$noHcube <- TRUE
-configJSON$inputWidgets[["_gmsopt_LstTitleLeftAligned"]] <- configJSON$inputWidgets[["_gmsopt_checkbox"]]
-configJSON$inputWidgets[["_gmsopt_LstTitleLeftAligned"]]$noHcube <- FALSE
-configJSON$inputWidgets[["_gmsopt_checkbox"]] <- NULL
-configJSON$outputAttachments <- list(list(
-  filename = "dowjones2016.csv",
-  execPerm = TRUE, throwError = FALSE
-))
-
-configJSON$scripts$hcube <- list(list(
-  title = "Test analysis",
-  id = "script1",
-  command = "gams",
-  args = c("test_script.gms", "--testVar", "test"),
-  outputFile = "out.txt"
-))
-
-jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
-
-if (file.exists(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))))) {
-  unlink(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))), force = TRUE)
-}
-
 test_that(
-  "Solving Hypercube jobs works",
-  {
+  "HC module works",
+  try_again(as.integer(Sys.getenv("MIRO_MAX_RETRIES", "3")), {
+    skip_if(
+      identical(Sys.getenv("ENGINE_URL"), ""),
+      "Skipping asynchronous solve tests as no ENGINE_URL was not set."
+    )
+    skip_if(
+      identical(Sys.getenv("ENGINE_USER"), ""),
+      "Skipping asynchronous solve tests as no ENGINE_USER was not set."
+    )
+    skip_if(
+      identical(Sys.getenv("ENGINE_PASSWORD"), ""),
+      "Skipping asynchronous solve tests as no ENGINE_PASSWORD was not set."
+    )
+    skip_if(
+      identical(Sys.getenv("ENGINE_NS"), ""),
+      "Skipping asynchronous solve tests as no ENGINE_NS was not set."
+    )
+
+    createTestDb()
+
+    apiURL <- Sys.getenv("ENGINE_URL")
+    inviterUser <- Sys.getenv("ENGINE_USER")
+    inviterPass <- Sys.getenv("ENGINE_PASSWORD")
+    namespace <- Sys.getenv("ENGINE_NS")
+
+    inviteeName <- paste0(Sys.getenv("ENGINE_USER"), "_", round(runif(1, 1, 100000)))
+
+    createUser(apiURL, inviterUser, inviterPass, namespace,
+      inviteeName, inviterPass,
+      volumeQuota = 68L
+    )
+
+    modelToTest <- "pickstock_configuration"
+    testModelDir <- file.path(testDir, "model", modelToTest)
+    modelDataPath <- file.path(testModelDir, paste0("data_", modelToTest))
+    configJSONFileName <- file.path(
+      testModelDir, paste0("conf_", modelToTest),
+      paste0(modelToTest, ".json")
+    )
+    # END setup
+
+    Sys.setenv(MIRO_MODEL_PATH = file.path(testModelDir, paste0(modelToTest, ".gms")))
+    Sys.setenv(MIRO_MODEL_NAME = modelToTest)
+    Sys.setenv(MIRO_REMOTE_EXEC = "true")
+    Sys.setenv("MIRO_REMOTE_EXEC_URL" = apiURL)
+    Sys.setenv("MIRO_REMOTE_EXEC_USERNAME" = inviteeName)
+    Sys.setenv("MIRO_REMOTE_EXEC_TOKEN" = getEngineToken(apiURL, inviteeName, inviterPass))
+    Sys.setenv("MIRO_REMOTE_EXEC_NS" = namespace)
+
+    # activate local upload module, deactivate
+    file.copy(file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), "_expected.json")),
+      file.path(dirname(configJSONFileName), paste0(tolower(modelToTest), ".json")),
+      overwrite = TRUE
+    )
+    configJSON <- suppressWarnings(jsonlite::fromJSON(configJSONFileName,
+      simplifyDataFrame = FALSE,
+      simplifyMatrix = FALSE
+    ))
+    configJSON$activateModules$loadLocal <- TRUE
+    configJSON$activateModules$hcube <- TRUE
+    configJSON$activateModules$attachments <- TRUE
+    configJSON$extraClArgs <- c(configJSON$extraClArgs, "--sleep=1")
+    configJSON$defCompMode <- "pivot"
+
+    configJSON$inputWidgets[["_gmspar_sliderrange"]]$noHcube <- FALSE
+    configJSON$inputWidgets[["trainingdays"]]$noHcube <- TRUE
+    configJSON$inputWidgets[["_gmsopt_LstTitleLeftAligned"]] <- configJSON$inputWidgets[["_gmsopt_checkbox"]]
+    configJSON$inputWidgets[["_gmsopt_LstTitleLeftAligned"]]$noHcube <- FALSE
+    configJSON$inputWidgets[["_gmsopt_checkbox"]] <- NULL
+    configJSON$outputAttachments <- list(list(
+      filename = "dowjones2016.csv",
+      execPerm = TRUE, throwError = FALSE
+    ))
+
+    configJSON$scripts$hcube <- list(list(
+      title = "Test analysis",
+      id = "script1",
+      command = "gams",
+      args = c("test_script.gms", "--testVar", "test"),
+      outputFile = "out.txt"
+    ))
+
+    jsonlite::write_json(configJSON, configJSONFileName, pretty = TRUE, auto_unbox = TRUE, null = "null")
+
+    if (file.exists(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))))) {
+      unlink(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))), force = TRUE)
+    }
+
+
     source(file.path(testDir, "shinytest", "hcube_module_solve_test.R"), local = TRUE)
-  }
-)
 
-test_that(
-  "Loading Hypercube jobs works",
-  {
+
+
     source(file.path(testDir, "shinytest", "hcube_module_load_test.R"), local = TRUE)
-  }
-)
 
-test_that(
-  "Compare Mode works with Hypercube jobs",
-  {
+
+
     source(file.path(testDir, "shinytest", "hcube_module_compare_test.R"), local = TRUE)
-  }
-)
 
-configJSON$hcubeWidgetGroups <- list(
-  list(
-    name = "test group 1",
-    members = c("clearvalueset", "solver")
-  ),
-  list(
-    name = "2nd tests group",
-    members = c("maxstock")
-  )
-)
 
-jsonlite::write_json(configJSON, configJSONFileName,
-  pretty = TRUE, auto_unbox = TRUE, null = "null"
-)
+    configJSON$hcubeWidgetGroups <- list(
+      list(
+        name = "test group 1",
+        members = c("clearvalueset", "solver")
+      ),
+      list(
+        name = "2nd tests group",
+        members = c("maxstock")
+      )
+    )
 
-if (file.exists(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))))) {
-  unlink(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))), force = TRUE)
-}
+    jsonlite::write_json(configJSON, configJSONFileName,
+      pretty = TRUE, auto_unbox = TRUE, null = "null"
+    )
 
-test_that(
-  "Hypercube widget groups work",
-  {
+    if (file.exists(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))))) {
+      unlink(file.path("~", ".miro", paste0(".cred_", tolower(modelToTest))), force = TRUE)
+    }
+
+
     source(file.path(testDir, "shinytest", "hcube_module_widget_groups_test.R"), local = TRUE)
-  }
+
+
+    removeUser(apiURL, inviterUser, inviterPass, inviteeName)
+
+    Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "MIRO_REMOTE_EXEC"))
+  })
 )
-
-removeUser(apiURL, inviterUser, inviterPass, inviteeName)
-
-Sys.unsetenv(c("MIRO_MODEL_PATH", "MIRO_DB_PATH", "MIRO_REMOTE_EXEC"))
