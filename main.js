7c830ef1b84f31acc012e10e59b6c362f88901e4
Frederik Proske <fproske@gams.com>
2024-04-10 16:20:27 +0200

    [Meta] Bump version number and date

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ const installRPackages = require('./components/install-r');
 const { refreshEngineJwt, isTokenExpired } = require('./components/engine');
 
 const requiredAPIVersion = 1;
-const miroVersion = '2.9.9999';
-const miroRelease = 'Feb 21 2024';
+const miroVersion = '2.9.1';
+const miroRelease = 'Apr 12 2024';
 const libVersion = '2.6';
 const exampleAppsData = require('./components/example-apps')(miroVersion, requiredAPIVersion);
 const LangParser = require('./components/LangParser');
--- src/app.R
+++ src/app.R
@@ -1,7 +1,7 @@
 # version number
-MIROVersion <- "2.9.9999"
+MIROVersion <- "2.9.1"
 APIVersion <- "1"
-MIRORDate <- "Feb 21 2024"
+MIRORDate <- "Apr 12 2024"
 
 MIROVersionString <<- paste0("GAMS MIRO v.", MIROVersion)
 
