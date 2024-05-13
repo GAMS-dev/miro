bad7cd7020e056167105679fa302dc298bfd1ad9
unknown <janinahasselbring@gmail.com>
2024-05-13 17:49:58 +0200

    [Examples] Working GAMSPy vrptw example

--- src/model/gamspy/vehicle_routing_tw/vehicle_routing_tw.py
+++ src/model/gamspy/vehicle_routing_tw/vehicle_routing_tw.py
@@ -18,7 +18,7 @@ from gamspy import (
     Sum,
     Variable,
     Ord,
-    Domain
+    Domain,
 )
 from gamspy.math import Max
 
@@ -26,7 +26,8 @@ from gamspy.math import Max
 def main():
     m = Container()
 
-    data = pd.read_csv("r101_solomon.txt", sep=",")
+    #data = pd.read_csv("r101_solomon.txt", sep=",")
+    data = pd.read_csv("small_data.txt", sep=",")
 
     # compute euclidean distance matrix between all customers
     coordinates = data[["XCOORD.", "YCOORD."]].to_numpy()
@@ -34,16 +35,19 @@ def main():
         np.einsum("ijk->ij", (coordinates[:, None, :] - coordinates) ** 2)
     )
 
-    vehicle_number = 25
+    # do lineariz the start time equation, where the slack is the allows timeframe at the depot
+    M = data["DUEDATE"][0]-data["READYTIME"][0]
+
+    vehicle_number = 10
     vehicle_capacity = 200
 
     # Set
     i = Set(m, name="i", records=data["CUSTNO."], description="customers")
     j = Alias(m, name="j", alias_with=i)
-    r = Set(
+    k = Set(
         m,
-        name="r",
-        records=["r" + str(i) for i in range(vehicle_number)],
+        name="k",
+        records=["k" + str(i) for i in range(vehicle_number)],
         description="vehicles",
     )
 
@@ -85,67 +89,79 @@ def main():
     )
 
     # Variable
-    a = Variable(
+    x = Variable(
         m,
-        name="a",
+        name="x",
         type="binary",
-        domain=[i, j, r],
-        description="states if vehicle r travels from ci to cj",
-    )
-    b = Variable(
-        m,
-        name="b",
-        type="binary",
-        domain=[i, r],
-        description="states if customer ci is serviced by vehicle r",
+        domain=[i, j, k],
+        description="states if vehicle k travels from ci to cj",
+        is_miro_output=True,
     )
     t = Variable(
         m,
         name="t",
+        type="positive",
         domain=[i],
-        description="start time of service for customer ci",
+        description="arrival time at customer ci",
+        is_miro_output=True,
     )
     w = Variable(
         m,
         name="w",
         type="positive",
         domain=[i],
-        description="waiting time of customer ci: ei-ti",
+        description="waiting time at customer ci: ei-ti",
+        is_miro_output=True,
     )
 
     # Equations
-    eq_arc_1 = Equation(
+    max_K_starts_at_depot = Equation(
         m,
-        name="eq_arc_1",
-        domain=[r, j],
-        description="exactly one arc leaves and enters each ci",
+        name="max_K_starts_at_depot",
+        domain=[i],
+        description="maximum K routes going out of the depot",
     )
-    eq_arc_2 = Equation(
+    start_at_depot = Equation(
+        m, domain=[k], name="start_at_depot", description="all routes start at depot"
+    )
+    end_at_depot = Equation(
+        m, domain=[k], name="end_at_depot", description="all routes end at depot"
+    )
+    each_node_enterd_once = Equation(
         m,
-        name="eq_arc_2",
-        domain=[r, i],
-        description="exactly one arc leaves and enters each ci",
+        name="each_node_enterd_once",
+        domain=[i],
+        description="exactly one arc enters each ci",
+    )
+    each_node_left_once = Equation(
+        m,
+        name="each_node_left_once",
+        domain=[j],
+        description="exactly one arc leaves each ci",
     )
     capacity = Equation(
         m,
         name="capacity",
-        domain=[r],
+        domain=[k],
         description="each vehicle must not be loaded with more than its capacity",
     )
-    only_one_visit = Equation(
+    start_time_depot = Equation(
         m,
-        name="only_one_visit",
+        name="start_time_depot",
         domain=[i],
-        description="each ci only served by one vehicle",
+        description="fix start time at depot to zero",
     )
-    start_at_depot = Equation(
-        m, name="start_at_depot", description="all routes start at depot"
+    waiting_time_depot = Equation(
+        m,
+        name="waiting_time_depot",
+        domain=[i],
+        description="fix waiting time at depot to zero",
     )
     start_time = Equation(
         m,
         name="start_time",
-        domain=[i, j],
-        description="defines the time at which customer ci starts to be serviced",
+        domain=[i, j, k],
+        description="defines the time windows",
     )
     serviced_after_earliest = Equation(
         m,
@@ -159,24 +175,29 @@ def main():
         domain=[i],
         description="check that the start_time is in the timeframe",
     )
-    waiting_time = Equation(
-        m,
-        name="waiting_time",
-        domain=[i],
-        description="waiting time of customer ci: ei-ti",
+
+    max_K_starts_at_depot[i] = (
+        Sum(Domain(j, k).where[Ord(j) > 1], x[i, j, k]) <= vehicle_number
+    )
+    start_at_depot[k] = Sum(j, x["0", j, k]) <= 1
+    end_at_depot[k] = Sum(j, x[j, "0", k]) <= 1
+    each_node_enterd_once[i].where[Ord(i) != 1] = (
+        Sum(Domain(j, k).where[Ord(i) != Ord(j)], x[i, j, k]) == 1
+    )
+    each_node_left_once[j].where[Ord(j) != 1] = Sum(Domain(i, k).where[Ord(i) != Ord(j)], x[i, j, k]) == 1
+    capacity[k] = (
+        Sum(Domain(i, j).where[Ord(i) != Ord(j)], x[i, j, k] * q[i]) <= vehicle_capacity
+    )
+    start_time_depot[i].where[Ord(i) == 1] = t[i] == 0
+    waiting_time_depot[i].where[Ord(i) == 1] = w[i] == 0
+    start_time[i, j, k].where[(Ord(i) != Ord(j)) & (Ord(j) != 1)] = (
+        t[i] + d[i, j] + s[i] + w[i] - t[j] <= (1 - x[i, j, k]) * M
     )
 
-    eq_arc_1[r,j] = Sum(i, a[i, j, r]) == b[j, r]
-    eq_arc_2[r,i] = Sum(j, a[i, j, r]) == b[i, r]
-    capacity[r] = Sum(i, b[i, r] * q[i]) <= vehicle_capacity
-    only_one_visit[i] = Sum(r, b[i, r]) == 1
-    start_at_depot[...] = Sum(Domain(r,i).where[Ord(i)==0], b[i, r]) == vehicle_number
-    start_time[i,j].where[Ord(i) != Ord(j)] = t[i] +  w[i] + s[i] + d[i, j] == t[j]
-    serviced_after_earliest[i] = e[i] <= t[i]
-    serviced_before_latest[i] = t[i] <= l[i]
-    waiting_time[i] = w[i] == e[i]-t[i]
+    serviced_after_earliest[i] = e[i] <= t[i] + w[i]
+    serviced_before_latest[i] = t[i] + w[i] <= l[i]
 
-    obj = Sum([i, j, r], a[i, j, r] * d[i, j])
+    obj = Sum(Domain(i, j, k).where[Ord(i) != Ord(j)], x[i, j, k] * d[i, j])
 
     vrptw = Model(
         m,
@@ -184,10 +205,9 @@ def main():
         equations=m.getEquations(),
         problem="MIP",
         sense=Sense.MIN,
-        objective=obj
+        objective=obj,
     )
     vrptw.solve(solver="CPLEX", output=sys.stdout)
-
 
 if __name__ == "__main__":
     main()
