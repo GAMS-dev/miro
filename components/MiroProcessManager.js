68bbfde70ad0c9048c9244c58009f9b286ec1a9c
Frederik Proske <fproske@gams.com>
2024-07-31 14:58:16 +0200

    [Launcher] Fix MIRO log output on Windows
    When running MIRO in development mode on the Windows terminal, newline characters were somehow gobbled and instead destroyed the following character output. I'm not sure why, but using `on('data')` and `process.stdout.write` instead seems to work.

--- components/MiroProcessManager.js
+++ components/MiroProcessManager.js
@@ -138,8 +138,6 @@ class MiroProcessManager {
 
     if (appData.stdOut != null) {
       stdOutPipe = appData.stdOut;
-    } else if (this.inDevelopmentMode) {
-      stdOutPipe = 'inherit';
     }
 
     let stdErrPipe = 'pipe';
@@ -243,6 +241,11 @@ developMode: ${this.inDevelopmentMode}, libPath: ${libPath}.`);
         } else {
           process.stderr.write(msg);
         }
+      });
+    }
+    if (this.inDevelopmentMode) {
+      this.miroProcesses[internalPid].stdout.on('data', (data) => {
+        process.stdout.write(data);
       });
     }
     this.miroProcesses[internalPid].catch(async (e) => {
