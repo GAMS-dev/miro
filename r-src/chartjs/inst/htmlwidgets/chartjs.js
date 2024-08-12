7697ac22367713386d74352d97c566e5b0109d01
Frederik Proske <fproske@gams.com>
2024-08-12 15:28:51 +0200

    [Meta] Fix multiple axes in chartjs

--- r-src/chartjs/inst/htmlwidgets/chartjs.js
+++ r-src/chartjs/inst/htmlwidgets/chartjs.js
@@ -1,30 +1,31 @@
 HTMLWidgets.widget({
+  name: "chartjs",
 
-  name: 'chartjs',
+  type: "output",
 
-  type: 'output',
-
-  initialize: function(el, width, height) {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      Chart.defaults.color = 'white';
+  initialize: function (el, width, height) {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      Chart.defaults.color = "white";
     } else {
-      Chart.defaults.color = '#666';
+      Chart.defaults.color = "#666";
     }
     return {};
   },
 
-  renderValue: function(el, x, instance) {
+  renderValue: function (el, x, instance) {
     if (x.debug) console.log(x);
     helpers = Chart.helpers;
 
-    var datasets = [];
     var data = [];
 
     // Final chart data
-    data  = {
+    data = {
       labels: x.data.labels,
       datasets: x.data.datasets,
-      legendTitle: x.legendTitle
+      legendTitle: x.legendTitle,
     };
 
     // Get element in page
@@ -41,70 +42,62 @@ HTMLWidgets.widget({
     var chartOptions = x.options;
 
     // Handle scales
-    if (x.scales != null){
-      chartOptions.scales = {};
-      if (x.scales.x != null) {
-        chartOptions.scales.x = x.scales.x;
-      }
-      if (x.scales.y != null) {
-        chartOptions.scales.y = x.scales.y;
-      }
+    if (x.scales != null) {
+      chartOptions.scales = x.scales;
     }
-//    chartOptions.dragData = true;
-//    chartOptions.dragX = false;
-//    chartOptions.dragDataRound = 6;
-//    chartOptions.dragOptions = {
-//      showTooltip: true
-//    };
-//    if(chartOptions.dragData === true){
-//      chartOptions.onDrag = function (e, datasetIndex, index, value) {
-//        // change cursor style to grabbing during drag action
-//        e.target.style.cursor = 'grabbing';
-//        // where e = event
-//      };
-//      chartOptions.onDragEnd = function (e, datasetIndex, index, value) {
-//        // restore default cursor style upon drag release
-//        e.target.style.cursor = 'default';
-//        // where e = event
-//        Shiny.onInputChange(el.id, {col: datasetIndex + 1, row: index + 1, value: value});
-//      };
-//      chartOptions.hover = {
-//        onHover: function(e) {
-//          // indicate that a datapoint is draggable by showing the 'grab' cursor when hovered
-//          const point = this.getElementAtEvent(e);
-//          if (point.length) e.target.style.cursor = 'grab';
-//          else e.target.style.cursor = 'default';
-//        }
-//      };
-//    }
+    //    chartOptions.dragData = true;
+    //    chartOptions.dragX = false;
+    //    chartOptions.dragDataRound = 6;
+    //    chartOptions.dragOptions = {
+    //      showTooltip: true
+    //    };
+    //    if(chartOptions.dragData === true){
+    //      chartOptions.onDrag = function (e, datasetIndex, index, value) {
+    //        // change cursor style to grabbing during drag action
+    //        e.target.style.cursor = 'grabbing';
+    //        // where e = event
+    //      };
+    //      chartOptions.onDragEnd = function (e, datasetIndex, index, value) {
+    //        // restore default cursor style upon drag release
+    //        e.target.style.cursor = 'default';
+    //        // where e = event
+    //        Shiny.onInputChange(el.id, {col: datasetIndex + 1, row: index + 1, value: value});
+    //      };
+    //      chartOptions.hover = {
+    //        onHover: function(e) {
+    //          // indicate that a datapoint is draggable by showing the 'grab' cursor when hovered
+    //          const point = this.getElementAtEvent(e);
+    //          if (point.length) e.target.style.cursor = 'grab';
+    //          else e.target.style.cursor = 'default';
+    //        }
+    //      };
+    //    }
 
     // Create actual chart
     instance.cjs = new Chart(ctx, {
-          type: x.type,
-          data: data,
-          options: chartOptions
-          });
+      type: x.type,
+      data: data,
+      options: chartOptions,
+    });
     if (canvas.onclick == null) {
       var lastClicked = null;
-      canvas.onclick = function() {
+      canvas.onclick = function () {
         if (lastClicked == null) {
           lastClicked = new Date().getTime();
         } else {
-          if (((new Date().getTime()) - lastClicked) < 500) {
+          if (new Date().getTime() - lastClicked < 500) {
             instance.cjs.resetZoom();
             lastClicked = null;
           } else {
             lastClicked = new Date().getTime();
           }
         }
-      }
+      };
     }
     if (x.debug) console.log(instance.cjs);
   },
 
-  resize: function(el, width, height, instance) {
-    if (instance.cjs)
-      instance.cjs.resize();
-  }
-
+  resize: function (el, width, height, instance) {
+    if (instance.cjs) instance.cjs.resize();
+  },
 });
