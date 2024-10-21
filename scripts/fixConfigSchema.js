ec5042bb1b96f250dfde1a73a6528492c1fdf4f9
rschuchmann <rschuchmann@gams.com>
2024-10-21 17:31:03 +0200

    Adapt scripts/fixConfigSchema.js to new schema structure

--- scripts/fixConfigSchema.js
+++ scripts/fixConfigSchema.js
@@ -136,7 +136,7 @@ const configSchema = JSON.parse(fs.readFileSync(configSchemaPath, 'utf8'));
 
 // make sure dashboard's dataViewsConfig is superset of miroPivotOptions
 // to allow user's copy&pasting views from MIRO pivot to dashboard views
-configSchema.definitions.dashboardOptions.properties.dataViewsConfig.additionalProperties.oneOf[1].properties =
+configSchema.definitions.dashboardOptions.properties.dataViewsConfig.additionalProperties.properties =
   mergeObjects(
     configSchema.definitions.miroPivotOptions.properties,
     configSchema.definitions.dashboardOptions.properties.dataViewsConfig
