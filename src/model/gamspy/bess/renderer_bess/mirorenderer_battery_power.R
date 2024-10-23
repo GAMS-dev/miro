45f474bb5f99613fd40a2c540b2764310e0e0196
Janina Hasselbring <janinahasselbring@gmail.com>
2024-10-23 15:09:05 +0200

    first try adding custom dashboard

---
+++ src/model/gamspy/bess/renderer_bess/mirorenderer_battery_power.R
@@ -0,0 +1,21 @@
+mirorenderer_battery_powerOutput <- function(id, height = NULL, options = NULL, path = NULL){
+    ns <- NS(id)
+plotOutput(ns("cumsumPlot"))
+}
+
+renderMirorenderer_battery_power <- function(input, output, session, data, options = NULL, path = NULL, rendererEnv = NULL, views = NULL, outputScalarsFull = NULL, ...){
+battery_power <- data$battery_power$level
+storage_level <- -cumsum(battery_power)
+
+max_storage <- data[["_scalarsve_out"]] %>%
+    filter(scalar == "battery_stoarge") %>%
+    pull(level)
+
+output$cumsumPlot <- renderPlot({
+    barplot(storage_level, col="lightblue", ylab="Energy Capacity in kWh" , 
+        names.arg=data$battery_power$j, las = 2, ylim=c(0,max_storage+10),
+        main = "Storage level of the BESS")
+    grid()
+})
+
+}
