3b57012dd41ea2d84409b1b8f065ba4f9d9d7de0
Janina Hasselbring <janinahasselbring@gmail.com>
2024-10-29 15:55:24 +0100

    add sankey

--- src/model/gamspy/bess/renderer_bess/mirorenderer_report_output.R
+++ src/model/gamspy/bess/renderer_bess/mirorenderer_report_output.R
@@ -1,36 +1,75 @@
-mirorenderer_report_outputOutput <- function(id, height = NULL, options = NULL, path = NULL){
-    ns <- NS(id)
-    tagList(
-        plotly::plotlyOutput(ns("sankey")),
-        sliderInput(ns("hour"), "Hour:",
-                  min = 0, max = 24,
-                  value = 0, step = 1)
-    )
-}
-
-renderMirorenderer_report_output <- function(input, output, session, data, options = NULL, path = NULL, rendererEnv = NULL, views = NULL, outputScalarsFull = NULL, ...){
-all_powers <- data
-print(filter(all_powers, power_output_header == "load_demand"))
-
-output$sankey <- plotly::renderPlotly({
-    plotly::plot_ly(
-        type = "sankey",
-        orientation = "h",
-        node = list(
-          label = c("A1", "A2", "B1", "B2", "C1", "C2"),
-          color = c("blue", "blue", "blue", "blue", "blue", "blue"),
-          pad = 15,
-          thickness = 20,
-          line = list(
-            color = "black",
-            width = 0.5
-          )
-        ),
-        link = list(
-          source = c(0,1,0,2,3,3),
-          target = c(2,3,3,4,4,5),
-          value =  c(8,4,2,8,4,2)
+mirorenderer_report_outputOutput <- function(id, height = NULL, options = NULL, path = NULL) {
+  ns <- NS(id)
+  tagList(
+    plotly::plotlyOutput(ns("sankey")),
+    sliderInput(ns("hour"), "Hour:",
+      min = 0, max = 24,
+      value = 0, step = 1
     )
   )
-})
+}
+
+renderMirorenderer_report_output <- function(input, output, session, data, options = NULL, path = NULL, rendererEnv = NULL, views = NULL, outputScalarsFull = NULL, ...) {
+  num_hours <- nrow(filter(data, power_output_header == "load_demand"))
+  updateSliderInput(session, "hour",
+    min = 0, max = num_hours - 1, step = 1
+  )
+
+  hour_to_dislay <- sprintf("hour%03d", input$hour)
+  sankey_source <- list()
+  sankey_target <- list()
+  sankey_value <- list()
+
+  battery_to_dislay <- filter(data, power_output_header == "battery") %>%
+    filter(j == hour_to_dislay)
+  gen_to_dislay <- filter(data, power_output_header == "generators") %>%
+    filter(j == hour_to_dislay)
+  extern_to_dislay <- filter(data, power_output_header == "external_grid") %>%
+    filter(j == hour_to_dislay)
+
+  if (dim(battery_to_dislay)[1] != 0) {
+    if (battery_to_dislay[["value"]] > 0) {
+      sankey_source <- c(sankey_source, 0)
+      sankey_target <- c(sankey_target, 3)
+      sankey_value <- c(sankey_value, battery_to_dislay[["value"]])
+    } else {
+      sankey_source <- c(sankey_source, 3)
+      sankey_target <- c(sankey_target, 0)
+      sankey_value <- c(sankey_value, -battery_to_dislay[["value"]])
+    }
+  }
+
+  if (dim(gen_to_dislay)[1] != 0) {
+    sankey_source <- c(sankey_source, 1)
+    sankey_target <- c(sankey_target, 3)
+    sankey_value <- c(sankey_value, gen_to_dislay[["value"]])
+  }
+
+  if (dim(extern_to_dislay)[1] != 0) {
+    sankey_source <- c(sankey_source, 2)
+    sankey_target <- c(sankey_target, 3)
+    sankey_value <- c(sankey_value, extern_to_dislay[["value"]])
+  }
+
+  output$sankey <- plotly::renderPlotly({
+    plotly::plot_ly(
+      type = "sankey",
+      orientation = "h",
+      node = list(
+        label = c("BESS", "Generators", "External Grid", "City"),
+        color = c("blue", "green", "red", "black"),
+        pad = 15,
+        thickness = 20,
+        line = list(
+          color = "black",
+          width = 0.5
+        )
+      ),
+      link = list(
+        source = sankey_source,
+        target = sankey_target,
+        value =  sankey_value
+      )
+    )
+  })
 }
