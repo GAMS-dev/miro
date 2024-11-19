2f92c9fe1ac09f58b0a18e63fe6deb496b012a97
rschuchmann <rschuchmann@gams.com>
2024-11-19 17:09:49 +0100

    [test] add dashboard tests

--- src/tests/shinytest/dashboard_test.R
+++ src/tests/shinytest/dashboard_test.R
@@ -4,7 +4,7 @@ app <- AppDriver$new("../../",
   timeout = as.integer(Sys.getenv("MIRO_TEST_TIMEOUT", "4000"))
 )
 rendererName <- Sys.getenv("DASHBOARD_RENDERER_NAME", "dashboard")
-getData <- function(id = paste0("tab_1_2-", rendererName, "-stockWeight2Table")) {
+getData <- function(id = paste0("tab_1_3-", rendererName, "-stockWeight2Table")) {
   return(jsonlite::fromJSON(app$get_values()$output[[id]])$x$data$datasets$data)
 }
 
@@ -12,23 +12,25 @@ Sys.sleep(2)
 app$click(selector = "a[data-value='outputData']")
 Sys.sleep(1)
 
+expect_true(identical(app$get_js(paste0("$('#tab_1_3-", rendererName, "-error_test')[0].innerText"), timeout = 50), "ERROR TEST\n79.61"))
+expect_true(identical(app$get_js(paste0("$('#tab_1_3-", rendererName, "-error_train')[0].innerText"), timeout = 50), "ERROR TRAIN\n951.17"))
 # switch data view
-expect_true(app$get_js(paste0("$('#tab_1_2-", rendererName, "-dowVSindexChart').is(':visible')")))
-expect_true(app$get_js(paste0("$('#tab_1_2-", rendererName, "-stockWeightChart').is(':hidden');")))
-app$click(selector = paste0("div[id='tab_1_2-", rendererName, "-error_train']"))
+expect_true(app$get_js(paste0("$('#tab_1_3-", rendererName, "-dowVSindexChart').is(':visible')")))
+expect_true(app$get_js(paste0("$('#tab_1_3-", rendererName, "-stockWeightChart').is(':hidden');")))
+app$click(selector = paste0("div[id='tab_1_3-", rendererName, "-error_train']"))
 Sys.sleep(1)
-expect_true(app$get_js(paste0("$('#tab_1_2-", rendererName, "-dowVSindexChart').is(':hidden')")))
-expect_true(app$get_js(paste0("$('#tab_1_2-", rendererName, "-stockWeightChart').is(':visible');")))
+expect_true(app$get_js(paste0("$('#tab_1_3-", rendererName, "-dowVSindexChart').is(':hidden')")))
+expect_true(app$get_js(paste0("$('#tab_1_3-", rendererName, "-stockWeightChart').is(':visible');")))
 
 # userFilter
-expect_equal(getData(paste0("tab_1_2-", rendererName, "-stockWeightChart")), list(c(0.60, 0.51)))
-expect_equal(getData(paste0("tab_1_2-", rendererName, "-stockWeight2Chart")), list(c(0.60, 0.51)))
+expect_equal(getData(paste0("tab_1_3-", rendererName, "-stockWeightChart")), list(c(0.60, 0.51)))
+expect_equal(getData(paste0("tab_1_3-", rendererName, "-stockWeight2Chart")), list(c(0.60, 0.51)))
 do.call(app$set_inputs, setNames(
   list("DD"),
-  paste0("tab_1_2-", rendererName, "-stockWeightuserFilter_symbol")
+  paste0("tab_1_3-", rendererName, "-stockWeightuserFilter_symbol")
 ))
 
 Sys.sleep(0.5)
-expect_equal(getData(paste0("tab_1_2-", rendererName, "-stockWeightChart")), list(0.6))
-expect_equal(getData(paste0("tab_1_2-", rendererName, "-stockWeight2Chart")), list(0.6))
+expect_equal(getData(paste0("tab_1_3-", rendererName, "-stockWeightChart")), list(0.6))
+expect_equal(getData(paste0("tab_1_3-", rendererName, "-stockWeight2Chart")), list(0.6))
 app$stop()
