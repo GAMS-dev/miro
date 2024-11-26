3aeaaaefa9d8dc929ad8681625b74e85f2830f96
Frederik Proske <fproske@gams.com>
2024-11-26 14:37:04 +0100

    [Server] Fix logging in auth container

--- server/auth/app/main.py
+++ server/auth/app/main.py
@@ -1,17 +1,9 @@
-import logging
-import sys
-
 from fastapi import FastAPI
 from fastapi.logger import logger
 
 from app.config import settings_yml, settings
 from app.routers import login, apps, scenarios, configuration
 from app.utils.utils import use_route_names_as_operation_ids
-
-# Configure the root log level and ensure all logs are sent to Gunicorn's error log.
-gunicorn_error_logger = logging.getLogger("gunicorn.error")
-logging.root.handlers.extend(gunicorn_error_logger.handlers)
-logging.root.setLevel(gunicorn_error_logger.level)
 
 app = FastAPI()
 
