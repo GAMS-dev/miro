45163d26a0f2db2b718fe6babdb5744d6bebd8d9
Frederik Proske <fproske@gams.com>
2024-11-27 13:16:37 +0100

    [Server] Fix logo not visible in kubernetes (proxy)

--- server/proxy/boot.sh
+++ server/proxy/boot.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+if [ -n "$GMS_MIRO_TARGET_IMG_DIR" ]; then
+    mkdir -p "$GMS_MIRO_TARGET_IMG_DIR"
+    for file in "/home/miroproxy/templates/2col/assets/img_default"/*; do
+        filename=$(basename "$file")
+        if [ ! -e "$GMS_MIRO_TARGET_IMG_DIR/$filename" ]; then
+            echo "Copying $filename to $GMS_MIRO_TARGET_IMG_DIR"
+            cp "$file" "$GMS_MIRO_TARGET_IMG_DIR/"
+        fi
+    done
+fi
+
 [[ -e data/application.yml ]] && cp data/application.yml .
 [[ -e data/logo.png ]] && cp data/logo.png templates/2col/assets/img/logo.png
 exec java -jar shinyproxy.jar
