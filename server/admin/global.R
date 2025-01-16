7cfbc199a4521765620bab677811bfc81fdcf647
Frederik Proske <fproske@gams.com>
2025-01-16 17:41:06 +0100

    [Meta] Bump version number and date

--- server/admin/global.R
+++ server/admin/global.R
@@ -14,7 +14,7 @@ IN_KUBERNETES <- Sys.getenv("KUBERNETES_SERVICE_HOST", "") != ""
 
 REQUIRED_API_VERSION <- 1
 MAX_LOGO_SIZE <- 1e6
-MIRO_VERSION <- "2.10.9999"
+MIRO_VERSION <- "2.11.0"
 MIRO_CONTAINER_DATA_DIR <- if (IN_KUBERNETES) "/home/miro/mnt/data" else "/home/miro/app/data"
 MIRO_MODEL_DIR <- file.path(getwd(), if (IN_KUBERNETES) "mnt" else "", "models")
 MIRO_DATA_DIR <- file.path(getwd(), if (IN_KUBERNETES) "mnt" else "", "data")
--- src/app.R
+++ src/app.R
@@ -1,7 +1,7 @@
 # version number
-MIROVersion <- "2.10.9999"
+MIROVersion <- "2.11.0"
 APIVersion <- "1"
-MIRORDate <- "Oct 29 2024"
+MIRORDate <- "Jan 24 2025"
 
 MIROVersionString <<- paste0("GAMS MIRO v.", MIROVersion)
 
