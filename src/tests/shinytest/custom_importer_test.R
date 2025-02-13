381bd80c716601af9f9da011b486c96ae94c82f3
Frederik Proske <fproske@gams.com>
2025-02-13 13:20:55 +0100

    [Load data] Fix NA to empty string conversion
    Output scalars and scalar variable/equations tables should keep NA instead of being converted to empty string.
    This also fixes an issue with output scalars incorrectly being reported as having data which leads to modified dataset count during local data import to be off by one.

--- src/tests/shinytest/custom_importer_test.R
+++ src/tests/shinytest/custom_importer_test.R
@@ -110,6 +110,56 @@ app$upload_file(externalSourceFile_3 = "../data/a.json")
 app$set_inputs(btImportExternal = "click")
 Sys.sleep(0.5)
 app$set_inputs(btReplaceInputData = "click")
-Sys.sleep(1)
+app$wait_for_js("($('#shiny-modal').data('bs.modal')||{}).isShown!==true", timeout = 10000L)
 expect_equivalent(getHotData(app, "in_1"), tibble(i = c("Seattle", "Boston"), value = c(123L, 456L)))
+expect_true(grepl("2 datasets imported",
+  app$get_js("$('.shiny-notification-content-text:visible').map(function(){return $(this).text();}).get().join(',')"),
+  fixed = TRUE
+))
+
+app$click(selector = "a[data-value='outputData']")
+Sys.sleep(1)
+app$set_inputs(outputTableView = "click")
+Sys.sleep(1)
+expect_equivalent(
+  getVisibleDtData(app, "table_tab_1_1-datatable"),
+  tibble(
+    ...1 = "Seattle", ...2 = "New-York",
+    ...3 = "100.123", ...4 = "123.456", ...5 = "321.432",
+    ...6 = "543.345", ...7 = "1", ...8 = "123", ...9 = "432"
+  )
+)
+app$set_inputs(outputTabset = "outputTabset_2")
+app$wait_for_js("$('#tab_1_2-scalarBoxes').text().trim().startsWith('NA')", timeout = 5000L)
+app$click(selector = "a[data-value='inputData']")
+
+app$click(selector = "#btRemove1")
+app$wait_for_js("($('.modal').data('bs.modal')||{}).isShown===true", timeout = 10000L)
+app$run_js("$('.modal .bt-gms-confirm').click()")
+app$wait_for_js("($('.modal').data('bs.modal')||{}).isShown!==true", timeout = 10000L)
+expect_equivalent(getHotData(app, "in_1"), tibble(i = "", value = "NA"))
+
+app$set_inputs(btImport = "click")
+Sys.sleep(0.5)
+app$set_inputs(tb_importData = "tb_importData_external")
+app$set_inputs(selExternalSource = "JSON import")
+Sys.sleep(0.5)
+app$upload_file(externalSourceFile_3 = "../data/a.json")
+app$set_inputs(cbSelectManuallyExt = TRUE)
+app$set_inputs(selInputDataExt = "a")
+app$set_inputs(btImportExternal = "click")
+Sys.sleep(0.5)
+app$wait_for_js("($('#shiny-modal').data('bs.modal')||{}).isShown!==true", timeout = 10000L)
+expect_equivalent(getHotData(app, "in_1"), tibble(i = c("Seattle", "Boston"), value = c(123L, 456L)))
+expect_true(grepl("1 datasets imported",
+  app$get_js("$('.shiny-notification-content-text:visible').map(function(){return $(this).text();}).get().join(',')"),
+  fixed = TRUE
+))
+app$click(selector = "a[data-value='outputData']")
+app$set_inputs(outputTabset = "outputTabset_1")
+Sys.sleep(1)
+app$set_inputs(outputTableView = "click")
+Sys.sleep(1)
+app$wait_for_js("$('#table_tab_1_1-noData').is(':visible');", timeout = 5000L)
+expect_true(app$get_js("$('#table_tab_1_1-data').is(':hidden');"))
 app$stop()
