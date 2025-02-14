563afdd99d80f8fabb5dae7502818f33b60a8646
Frederik Proske <fproske@gams.com>
2025-02-14 14:40:21 +0100

    [Meta] Bump release date and version to 2.11.1

--- src/app.R
+++ src/app.R
@@ -1,7 +1,7 @@
 # version number
-MIROVersion <- "2.11.0"
+MIROVersion <- "2.11.1"
 APIVersion <- "1"
-MIRORDate <- "Jan 21 2025"
+MIRORDate <- "Feb 19 2025"
 
 MIROVersionString <<- paste0("GAMS MIRO v.", MIROVersion)
 
