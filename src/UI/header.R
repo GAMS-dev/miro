GAMS-dev/miro

commit 90b0ce8489dd62f7308cb39ce634902f63d700de

[UI] Remove some unecessary `HTML()` calls

src/UI/header.R
@@ -8,17 +8,28 @@ header <- dashboardHeader(
     ),
     tags$ul(
       class = "dropdown-menu", role = "menu",
-      tags$li(actionLink("btRemoveDuplicates", HTML(paste0('<div class="menu-icon-align"><i class="fa fa-diagram-next"></i></div> ', lang$nav$header$scenario$removeDuplicates)))),
+      tags$li(actionLink("btRemoveDuplicates", tagList(tags$div(class = "menu-icon-align", tags$i(class = "fa fa-diagram-next")), lang$nav$header$scenario$removeDuplicates))),
       if (!config$activateModules$readonlyMode) tags$li(actionLink("btEditMeta", HTML(paste0('<div class="menu-icon-align"><i class="fa fa-magnifying-glass"></i></div> ', lang$nav$header$scenario$edit)))),
       if (!config$activateModules$readonlyMode) tags$li(actionLink("btSave", HTML(paste0('<div class="menu-icon-align"><i class="fa fa-floppy-disk"></i></div> ', lang$nav$header$scenario$save)))),
       if (!config$activateModules$readonlyMode) tags$li(actionLink("btSaveAs", HTML(paste0('<div class="menu-icon-align"></div> ', lang$nav$header$scenario$saveAs)))),
-      tags$li(HTML(paste0(
-        '<a href="#" class="action-button"
-                                      onclick="Shiny.setInputValue(\'btExportScen\', 1, {priority: \'event\'})">
-                                      <div class="menu-icon-align"><i class="fa fa-file-export"></i></div> ',
-        lang$nav$header$scenario$export, "</a>"
-      ))),
-      if (!config$activateModules$readonlyMode) tags$li(actionLink("btDelete", HTML(paste0('<div class="menu-icon-align"><i class="fa fa-trash"></i></div> ', lang$nav$header$scenario$delete))))
+      tags$li(tags$a(
+        href = "#", class = "action-button",
+        onclick = "Shiny.setInputValue(\'btExportScen\', 1, {priority: \'event\'})",
+        tags$div(
+          class = "menu-icon-align", tags$i(class = "fa fa-file-export"),
+          lang$nav$header$scenario$export
+        )
+      )),
+      if (!config$activateModules$readonlyMode) {
+        tags$li(actionLink(
+          "btDelete",
+          tags$div(
+            class = "menu-icon-align",
+            tags$i(class = "fa fa-trash"),
+            lang$nav$header$scenario$delete
+          )
+        ))
+      }
     )
   ),
   tags$li(

commit d1dc31d29b9c9c8a4d975a2eef81c1ffd4cd0ce8

[UI] Fix alignment in header in scenario menu
This got broken by 90b0ce84

src/UI/header.R
@@ -14,20 +14,20 @@ header <- dashboardHeader(
       if (!config$activateModules$readonlyMode) tags$li(actionLink("btSaveAs", HTML(paste0('<div class="menu-icon-align"></div> ', lang$nav$header$scenario$saveAs)))),
       tags$li(tags$a(
         href = "#", class = "action-button",
-        onclick = "Shiny.setInputValue(\'btExportScen\', 1, {priority: \'event\'})",
+        onclick = "Shiny.setInputValue('btExportScen', 1, {priority: 'event'})",
         tags$div(
-          class = "menu-icon-align", tags$i(class = "fa fa-file-export"),
-          lang$nav$header$scenario$export
-        )
+          class = "menu-icon-align", tags$i(class = "fa fa-file-export")
+        ),
+        lang$nav$header$scenario$export
       )),
       if (!config$activateModules$readonlyMode) {
-        tags$li(actionLink(
-          "btDelete",
+        tags$li(tags$a(
+          href = "#", class = "action-button",
+          id = "btDelete",
           tags$div(
-            class = "menu-icon-align",
-            tags$i(class = "fa fa-trash"),
-            lang$nav$header$scenario$delete
-          )
+            class = "menu-icon-align", tags$i(class = "fa fa-trash")
+          ),
+          lang$nav$header$scenario$delete
         ))
       }
     )