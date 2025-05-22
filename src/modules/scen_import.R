GAMS-dev/miro

commit faaf570bcd71da291483c8e1f3142e3968936abb

[Import] Increase log verbosity

src/modules/scen_import.R
@@ -580,8 +580,10 @@ observeEvent(virtualActionButton(rv$btOverwriteInput), {
   newDataCount <- newInputCount + newOutputCount
 
   if (newDataCount) {
+    flog.debug("%d datasets imported (loadMode: %s)", newDataCount, loadMode)
     showNotification(sprintf(lang$nav$notificationNewInput$new, newDataCount))
   } else {
+    flog.debug("No dataset imported (loadMode: %s)", loadMode)
     showNotification(lang$nav$notificationNewInput$noNew, type = "error")
   }
   if (length(loadErrors)) {
@@ -676,8 +678,10 @@ loadDatasetsIntoSandbox <- function() {
   }
   scenData$loadSandbox(scenInputData, names(scenInputData), activeScen$getMetadataDf())
   if (newInputCount) {
+    flog.debug("%d datasets imported from database into sandbox", newDataCount)
     showNotification(sprintf(lang$nav$notificationNewInput$new, newInputCount))
   } else {
+    flog.debug("No dataset imported from database into sandbox")
     showNotification(lang$nav$notificationNewInput$noNew, type = "error")
   }
 }

commit a5ba4ce6d5206874ebfcc2322910b24d32663ec6

[UI] Fix crash when importing from db
Bad debug log introduced with faaf570bcd71da291483c8e1f3142e3968936abb

src/modules/scen_import.R
@@ -678,7 +678,7 @@ loadDatasetsIntoSandbox <- function() {
   }
   scenData$loadSandbox(scenInputData, names(scenInputData), activeScen$getMetadataDf())
   if (newInputCount) {
-    flog.debug("%d datasets imported from database into sandbox", newDataCount)
+    flog.debug("%d datasets imported from database into sandbox", newInputCount)
     showNotification(sprintf(lang$nav$notificationNewInput$new, newInputCount))
   } else {
     flog.debug("No dataset imported from database into sandbox")