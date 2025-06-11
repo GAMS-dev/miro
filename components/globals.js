b4806651a93ca64aaaeb8eb3dfc2362f3c553681
Frederik Proske <fproske@gams.com>
2025-06-11 18:15:55 +0200

    [Meta] Bump version to 2.12.0

--- components/globals.js
+++ components/globals.js
@@ -1,6 +1,6 @@
 const apiVersion = 1;
-const miroVersion = '2.11.9999';
-const miroRelease = 'May 22 2025';
+const miroVersion = '2.12.0';
+const miroRelease = 'Jun 13 2025';
 const libVersion = '2.6';
 
 export { apiVersion, miroVersion, miroRelease, libVersion };
