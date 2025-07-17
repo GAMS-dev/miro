af1b4ff96e9a71394d539475815f417fec176370
Frederik Proske <fproske@gams.com>
2025-07-17 16:11:21 +0200

    [Tests] Improve robustness of MIRO Pivot UI test

--- src/tests/shinytest/miropivot_test.R
+++ src/tests/shinytest/miropivot_test.R
@@ -4,6 +4,7 @@ app <- AppDriver$new("../../",
   timeout = as.integer(Sys.getenv("MIRO_TEST_TIMEOUT", "4000"))
 )
 getData <- function(id = "tab_1_1") {
+  app$wait_for_js(paste0("Chart.getChart('", id, "-miroPivot-pivotChart')!=null"), timeout = 5000L)
   return(jsonlite::fromJSON(app$get_values()$output[[paste0(id, "-miroPivot-pivotChart")]])$x$data$datasets$data)
 }
 Sys.sleep(2)
