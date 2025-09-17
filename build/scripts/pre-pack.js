caa47c7be613c43ab672b562b0cbf4cb69d4beff
Frederik Proske <fproske@gams.com>
2025-09-17 12:05:46 +0200

    [CI] Re-install R if exists but old version

--- build/scripts/pre-pack.js
+++ build/scripts/pre-pack.js
@@ -4,13 +4,6 @@ import fs from 'fs-extra';
 import { execa } from 'execa';
 import https from 'node:https';
 
-let rExists = false;
-if (!fs.existsSync(path.join('.', 'r'))) {
-  fs.mkdirSync(path.join('.', 'r'));
-} else {
-  console.log('R already exists. Skipping installation.');
-  rExists = true;
-}
 const buildDocker = process.argv[2] === '--docker';
 
 const buildConfig = JSON.parse(fs.readFileSync('build-config.json'));
@@ -39,53 +32,92 @@ const tryInstallRPackages = async () => {
     process.exit(1);
   }
 };
+const getRVersion = async (rPath) => {
+  try {
+    const { stdout } = await execa(rPath, ['-e', 'R.version.string']);
+    const version = stdout.split(' ')[3];
+    return version;
+  } catch (error) {
+    console.error('Error getting R version:', error);
+    return null;
+  }
+};
 (async () => {
+  let rExists = false;
+  if (!fs.existsSync(path.join('.', 'r'))) {
+    fs.mkdirSync(path.join('.', 'r'));
+  } else {
+    let rPath = 'Rscript';
+    if (process.platform === 'win32') {
+      rPath = path.join('.', 'r', 'bin', 'Rscript');
+    }
+    const installedRVersion = await getRVersion(rPath);
+    if (installedRVersion === buildConfig.rVersion) {
+      console.log('R already exists. Skipping installation.');
+      rExists = true;
+    } else {
+      console.log(
+        `R already exists but with a different version (${installedRVersion}) than in build config (${buildConfig.rVersion}). Reinstalling...`,
+      );
+    }
+  }
   if (process.platform === 'win32' && !rExists) {
     try {
       console.log('Installing R...');
       const file = fs.createWriteStream(path.join('r', 'latest_r.exe'));
-      https.get(`https://cloud.r-project.org/bin/windows/base/R-${buildConfig.rVersion}-win.exe`, (response) => {
-        response.pipe(file);
+      https
+        .get(
+          `https://cloud.r-project.org/bin/windows/base/R-${buildConfig.rVersion}-win.exe`,
+          (response) => {
+            response.pipe(file);
 
-        file.on('finish', () => {
-          file.close(async () => {
-            const subproc = execa(
-              'innoextract',
-              ['-e', 'latest_r.exe'],
-              { cwd: path.join('.', 'r') },
-            );
-            subproc.stderr.pipe(process.stderr);
-            subproc.stdout.pipe(process.stderr);
-            await subproc;
-            try {
-              await fs.move(path.join('.', 'r', 'app'), path.join('.', 'r-tmp'), {
-                overwrite: true,
+            file.on('finish', () => {
+              file.close(async () => {
+                const subproc = execa('innoextract', ['-e', 'latest_r.exe'], {
+                  cwd: path.join('.', 'r'),
+                });
+                subproc.stderr.pipe(process.stderr);
+                subproc.stdout.pipe(process.stderr);
+                await subproc;
+                try {
+                  await fs.move(
+                    path.join('.', 'r', 'app'),
+                    path.join('.', 'r-tmp'),
+                    {
+                      overwrite: true,
+                    },
+                  );
+                  await fs.move(path.join('.', 'r-tmp'), path.join('.', 'r'), {
+                    overwrite: true,
+                  });
+                } catch (e) {
+                  console.log(
+                    `Problems moving R. Error message: ${e.message}`,
+                  );
+                  fs.remove(path.join('.', 'r')).catch((err) => {
+                    console.error(err);
+                  });
+                  process.exit(1);
+                }
+                try {
+                  await tryInstallRPackages();
+                } catch (e) {
+                  console.log(
+                    `Problems installing R packages. Error message: ${e.message}`,
+                  );
+                  process.exit(1);
+                }
               });
-              await fs.move(path.join('.', 'r-tmp'), path.join('.', 'r'), {
-                overwrite: true,
-              });
-            } catch (e) {
-              console.log(`Problems moving R. Error message: ${e.message}`);
-              fs.remove(path.join('.', 'r')).catch((err) => {
-                console.error(err);
-              });
-              process.exit(1);
-            }
-            try {
-              await tryInstallRPackages();
-            } catch (e) {
-              console.log(`Problems installing R packages. Error message: ${e.message}`);
-              process.exit(1);
-            }
+            });
+          },
+        )
+        .on('error', async (e) => {
+          console.log(`Problems installing R. Error message: ${e.message}`);
+          fs.remove(path.join('.', 'r')).catch((err) => {
+            console.error(err);
           });
+          process.exit(1);
         });
-      }).on('error', async (e) => {
-        console.log(`Problems installing R. Error message: ${e.message}`);
-        fs.remove(path.join('.', 'r')).catch((err) => {
-          console.error(err);
-        });
-        process.exit(1);
-      });
     } catch (e) {
       console.log(`Problems installing R. Error message: ${e.message}`);
       fs.remove(path.join('.', 'r')).catch((err) => {
@@ -116,29 +148,49 @@ const tryInstallRPackages = async () => {
     try {
       await tryInstallRPackages();
     } catch (e) {
-      console.log(`Problems installing R packages. Error message: ${e.message}`);
+      console.log(
+        `Problems installing R packages. Error message: ${e.message}`,
+      );
       process.exit(1);
     }
   }
   if (buildDocker && process.argv[3] === '--ci') {
     try {
       console.log('Building CI Docker image...');
-      let subprocCi = execa('docker', ['build',
-        '--build-arg', `GAMS_MAJOR=${buildConfig.gamsVersion.split('.')[0]}`,
-        '--build-arg', `GAMS_MINOR=${buildConfig.gamsVersion.split('.')[1]}`,
-        '--build-arg', `GAMS_MAINT=${buildConfig.gamsVersion.split('.')[2]}`,
-        '--build-arg', `R_BASE_VERSION=${buildConfig.rVersion}`,
-        '-t', 'gamsmiro-ci', '-f', 'ci/Dockerfile', '.']);
+      let subprocCi = execa('docker', [
+        'build',
+        '--build-arg',
+        `GAMS_MAJOR=${buildConfig.gamsVersion.split('.')[0]}`,
+        '--build-arg',
+        `GAMS_MINOR=${buildConfig.gamsVersion.split('.')[1]}`,
+        '--build-arg',
+        `GAMS_MAINT=${buildConfig.gamsVersion.split('.')[2]}`,
+        '--build-arg',
+        `R_BASE_VERSION=${buildConfig.rVersion}`,
+        '-t',
+        'gamsmiro-ci',
+        '-f',
+        'ci/Dockerfile',
+        '.',
+      ]);
       subprocCi.stderr.pipe(process.stderr);
       subprocCi.stdout.pipe(process.stderr);
       await subprocCi;
-      subprocCi = execa('docker', ['build',
-        '-t', 'gamsmiro-server-ci', '-f', 'ci/Dockerfile-server-tests', '.']);
+      subprocCi = execa('docker', [
+        'build',
+        '-t',
+        'gamsmiro-server-ci',
+        '-f',
+        'ci/Dockerfile-server-tests',
+        '.',
+      ]);
       subprocCi.stderr.pipe(process.stderr);
       subprocCi.stdout.pipe(process.stderr);
       await subprocCi;
     } catch (e) {
-      console.log(`Problems building CI Docker image. Error message: ${e.message}`);
+      console.log(
+        `Problems building CI Docker image. Error message: ${e.message}`,
+      );
       process.exit(1);
     }
   }
