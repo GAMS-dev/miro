624ab91c1579d23f680d9d5e10a998a240f1579f
Frederik Proske <fproske@gams.com>
2026-02-23 14:01:31 +0100

    [Meta] Bump release date

--- components/globals.js
+++ components/globals.js
@@ -1,6 +1,6 @@
 const apiVersion = 1;
 const miroVersion = '2.13.4';
-const miroRelease = 'Feb 19 2026';
+const miroRelease = 'Feb 24 2026';
 const libVersion = '2.13';
 
 export { apiVersion, miroVersion, miroRelease, libVersion };
--- src/app.R
+++ src/app.R
@@ -1,7 +1,7 @@
 # version number
 MIROVersion <- "2.13.4"
 APIVersion <- "1"
-MIRORDate <- "Feb 19 2026"
+MIRORDate <- "Feb 24 2026"
 
 MIROVersionString <<- paste0("GAMS MIRO v.", MIROVersion)
 
