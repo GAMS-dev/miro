44cad1c75b83371abf317a7dcbee7469375c8414
Frederik Proske <fproske@gams.com>
2026-06-12 17:02:44 +0200

    [Launcher] Attempt to prevent keychain prompt on macOS

--- main.js
+++ main.js
@@ -37,6 +37,8 @@ import {
 } from './components/globals.js';
 
 const { TouchBarButton, TouchBarSpacer } = TouchBar;
+app.commandLine.appendSwitch('password-store', 'basic');
+app.commandLine.appendSwitch('use-mock-keychain');
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
